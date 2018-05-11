d9f92ac651eabe3275b05e93b1470931da6349ef
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-05-11 11:52:52 +0300

    Add logging of exceptions for each net-related thread

--- Thundermint/Logger.hs
+++ Thundermint/Logger.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE DeriveFunctor              #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
 -- |
 -- Logger 
 module Thundermint.Logger (
     MonadLogger(..)
   , LoggerT(..)
   , runLoggerT
+  , logOnException
+    -- * Reexports
+  , Severity(..)
   ) where
 
 import Control.Arrow (first,second)
@@ -13,6 +17,9 @@ import Control.Monad.Catch
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader
+import Control.Exception          (Exception,SomeException(..),AsyncException(..))
+import Data.Typeable
+import Data.Monoid     ((<>))
 import Katip
 
 import Thundermint.Control
@@ -24,7 +31,9 @@ import Thundermint.Control
 -- | Monad which supports logging. Unlike 'Katip' it ties namespace to
 --   the monad.
 class MonadIO m => MonadLogger m where
+  -- | Log value
   logger :: LogItem a => Severity -> LogStr -> a -> m ()
+  -- | Change current namespace
   localNamespace :: (Namespace -> Namespace) -> m a -> m a
 
 -- | Concrete implementation of logger monad
@@ -44,3 +53,15 @@ instance MonadIO m => MonadLogger (LoggerT m) where
     (nm,_) <- LoggerT ask
     logF a nm sev s
   localNamespace f (LoggerT m) = LoggerT $ local (first f) m
+
+-- | Log exceptions at Error severity
+logOnException :: (MonadLogger m, MonadCatch m) => m a -> m a
+logOnException = handle logE
+  where
+    logE e
+      | Just ThreadKilled <- fromException e = do
+          logger InfoS "Killed normally by ThreadKilled" ()
+          throwM e
+      | SomeException eTy <- e               = do
+          logger ErrorS ("Killed by " <> showLS e <> " :: " <> showLS (typeOf eTy)) ()
+          throwM e
