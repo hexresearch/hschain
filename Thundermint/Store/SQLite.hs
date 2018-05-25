14e24985223d6c5050d9bc42b51c1dcc1c5b7a74
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-05-25 12:11:28 +0300

    Protect SQLite access with mutex

--- Thundermint/Store/SQLite.hs
+++ Thundermint/Store/SQLite.hs
@@ -1,13 +1,15 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE OverloadedStrings #-}
 -- |
 module Thundermint.Store.SQLite (
   newSQLiteBlockStorage
   ) where
 
 import Codec.Serialise (Serialise,serialise,deserialiseOrFail)
+import Control.Concurrent.MVar
 import Data.Int
 import qualified Database.SQLite.Simple as SQL
 import           Database.SQLite.Simple   (Only(..))
@@ -24,24 +26,34 @@ newSQLiteBlockStorage
   -> Block alg a
   -> IO (BlockStorage 'RW IO alg a)
 newSQLiteBlockStorage dbFile gBlock = do
-  conn <- SQL.open dbFile
-  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS blockchain (height INT NOT NULL UNIQUE, bid BLOB NOT NULL, block BLOB NOT NULL)"
-  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS commits (height INT NOT NULL UNIQUE, cmt BLOB NOT NULL)"
+  mutex <- newMutex
+  conn  <- SQL.open dbFile
+  -- Initialize tables
+  SQL.execute_ conn
+    "CREATE TABLE IF NOT EXISTS blockchain \
+    \  ( height INT NOT NULL UNIQUE \
+    \  , bid    BLOB NOT NULL \
+    \  , block  BLOB NOT NULL)"
+  SQL.execute_ conn
+    "CREATE TABLE IF NOT EXISTS commits \
+    \  ( height INT NOT NULL UNIQUE \
+    \  , cmt    BLOB NOT NULL)"
+  -- Insert genesis block if needed
   SQL.query_ conn "SELECT MAX(height) FROM blockchain" >>= \case
     [Only Nothing] -> SQL.execute conn "INSERT INTO blockchain VALUES (?,?,?)"
-                        (0::Int64
+                        ( 0 :: Int64
                         , serialise (blockHash gBlock :: BlockID alg a)
                         , serialise gBlock)
     (_::[Only (Maybe Int64)]) -> return ()
-  --
-  
+  -- Make dictionary
   return BlockStorage
-    { blockchainHeight = SQL.query_ conn "SELECT MAX(height) FROM blockchain" >>= \case
-        []       -> error "Blockchain cannot be empty"
-        [Only i] -> return (Height i)
-        _        -> error "Impossible"
+    { blockchainHeight = withMutex mutex $
+        SQL.query_ conn "SELECT MAX(height) FROM blockchain" >>= \case
+          []       -> error "Blockchain cannot be empty"
+          [Only i] -> return (Height i)
+          _        -> error "Impossible"
     --
-    , retrieveBlock = \(Height h) ->
+    , retrieveBlock = \(Height h) -> withMutex mutex $
         SQL.query conn "SELECT block FROM blockchain WHERE height = ?" (Only h) >>= \case
           [] -> return Nothing
           [Only bs] -> case deserialiseOrFail bs of
@@ -49,7 +61,7 @@ newSQLiteBlockStorage dbFile gBlock = do
             Left  e -> error (show e)
           _         -> error "Impossible"
     --
-    , retrieveBlockID = \(Height h) ->
+    , retrieveBlockID = \(Height h) -> withMutex mutex $
         SQL.query conn "SELECT bid FROM blockchain WHERE height = ?" (Only h) >>= \case
           [] -> return Nothing
           [Only bs] -> case deserialiseOrFail bs of
@@ -57,7 +69,7 @@ newSQLiteBlockStorage dbFile gBlock = do
             Left  e -> error (show e)
           _         -> error "Impossible"
     --
-    , retrieveCommit = \(Height h) -> do
+    , retrieveCommit = \(Height h) -> withMutex mutex $
         SQL.query conn "SELECT cmt FROM commits WHERE height = ?" (Only h) >>= \case
           [] -> return Nothing
           [Only bs] -> case deserialiseOrFail bs of
@@ -65,7 +77,7 @@ newSQLiteBlockStorage dbFile gBlock = do
             Left  e -> error (show e)
           _         -> error "Impossible"
     --
-    , retrieveLastCommit = do
+    , retrieveLastCommit = withMutex mutex $
         SQL.query_ conn "SELECT cmt FROM commits ORDER BY height DESC LIMIT 1" >>= \case
           [] -> return Nothing
           [Only bs] -> case deserialiseOrFail bs of
@@ -73,7 +85,7 @@ newSQLiteBlockStorage dbFile gBlock = do
             Left  e -> error (show e)
           _         -> error "Impossible"
     --
-    , storeCommit = \cmt blk -> do
+    , storeCommit = \cmt blk -> withMutex mutex $ do
         let Height h = headerHeight $ blockHeader blk
         SQL.execute_ conn "BEGIN TRANSACTION"
         SQL.execute conn "INSERT INTO commits VALUES (?,?)" (h, serialise cmt)
@@ -82,5 +94,14 @@ newSQLiteBlockStorage dbFile gBlock = do
           , serialise (blockHash blk :: BlockID alg a)
           , serialise blk
           )
-        SQL.execute_ conn "COMMIT"    
+        SQL.execute_ conn "COMMIT"
     }
+
+
+newtype Mutex = Mutex (MVar ())
+
+newMutex :: IO Mutex
+newMutex = Mutex <$> newMVar ()
+
+withMutex :: Mutex -> IO a -> IO a
+withMutex (Mutex mvar) = withMVar mvar . const
