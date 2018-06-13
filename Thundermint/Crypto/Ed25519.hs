1470c85b71146057cd5ca5fb4b7f51f396ab26be
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-06-13 21:11:37 +0300

    Add CBOR instances for validators and sets

--- Thundermint/Crypto/Ed25519.hs
+++ Thundermint/Crypto/Ed25519.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies      #-}
@@ -6,7 +7,8 @@ module Thundermint.Crypto.Ed25519 where
 
 import Thundermint.Crypto
 
-import Crypto.Error        (throwCryptoError)
+import qualified Codec.Serialise as CBOR
+import Crypto.Error        (throwCryptoError,CryptoFailable(..))
 import Crypto.Hash         (Digest, SHA512, hash)
 import Crypto.Random.Types (MonadRandom)
 import Data.ByteArray      (ByteArrayAccess, convert)
@@ -24,17 +26,26 @@ data Ed25519_SHA512
 
 newtype instance PrivKey   Ed25519_SHA512 = PrivKey Ed.SecretKey
 newtype instance PublicKey Ed25519_SHA512 = PublicKey Ed.PublicKey
---newtype instance Signature Ed25519_SHA512 = Ed25519_SHA512Sig Ed.Signature
+
+instance CBOR.Serialise (PublicKey Ed25519_SHA512) where
+  encode (PublicKey pk) = CBOR.encode (convert pk :: ByteString)
+  decode = do bs <- CBOR.decode
+              case Ed.publicKey (bs :: ByteString) of
+                CryptoPassed pk -> return (PublicKey pk)
+                CryptoFailed e  -> fail (show e)
+
 
 -- | We assume that there's
 instance Crypto Ed25519_SHA512 where
   signBlob (PrivKey k)  = Signature . convert . Ed.sign k pubKey
    where pubKey = Ed.toPublic k
 
-  verifyBlobSignature (PublicKey pubKey) blob (Signature s) = Ed.verify pubKey blob (throwCryptoError $ Ed.signature s)
-  publicKey (PrivKey k)  = PublicKey $ Ed.toPublic k
-  address   (PublicKey  k)  = Address $ convert k
-  hashBlob                  = Hash . sha512
+  verifyBlobSignature (PublicKey pubKey) blob (Signature s)
+    = Ed.verify pubKey blob (throwCryptoError $ Ed.signature s)
+
+  publicKey (PrivKey k)   = PublicKey $ Ed.toPublic k
+  address   (PublicKey k) = Address $ convert k
+  hashBlob                = Hash . sha512
 
 generatePrivKey :: MonadRandom m => m (PrivKey Ed25519_SHA512)
 generatePrivKey = fmap PrivKey Ed.generateSecretKey
