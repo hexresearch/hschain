2632ceacfba8db9f9fcb42126ecd7518568252b6
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-06-26 20:18:31 +0300

    Actually store proposed blocks
    They were silently rejected by proposal storage when they were
    generated but accepted on second attempt that led to requiring to make
    full round trip through all proposers. In effect it led to ~10x slowdown
    
    And only worked because block generation is strictly deterministic

--- Thundermint/Consensus/Algorithm.hs
+++ Thundermint/Consensus/Algorithm.hs
@@ -88,7 +88,7 @@ data HeightParameres (m :: * -> *) alg a = HeightParameres
     -- ^ Broadcast to peers announcement that we have given precommit
   , announceStep         :: FullStep -> m ()
 
-  , createProposal       :: Maybe (Commit alg a) -> m (BlockID alg a)
+  , createProposal       :: Round -> Maybe (Commit alg a) -> m (BlockID alg a)
     -- ^ Create new proposal block. Block itself should be stored
     --   elsewhere.
   , commitBlock          :: forall x. Commit alg a -> m x
@@ -297,7 +297,7 @@ enterPropose par@HeightParameres{..} r sm@TMState{..} = do
     Just (br,bid) -> do logger InfoS ("Making proposal: " <> showLS bid) ()
                         broadcastProposal r bid (Just (br,bid))
     -- Otherwise we need to create new block from mempool
-    Nothing      -> do bid <- createProposal smLastCommit
+    Nothing      -> do bid <- createProposal r smLastCommit
                        logger InfoS ("Making proposal: " <> showLS bid) ()
                        broadcastProposal r bid Nothing
   return sm { smRound = r
