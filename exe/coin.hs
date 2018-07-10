ca1e872a57e768cb8eeea8937b7c0bcfcaa40679
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-07-10 15:29:35 +0300

    Move logic for coin into separate module
    This should allow for code sharing between different programs
    working with same blockchain

--- exe/coin.hs
+++ exe/coin.hs
@@ -32,6 +32,7 @@ import Thundermint.Store
 import Thundermint.Store.STM
 import Thundermint.Store.SQLite
 import Thundermint.Mock
+import Thundermint.Mock.Coin
 
 
 ----------------------------------------------------------------
@@ -57,83 +58,7 @@ walletKeys = makePrivateValidators
 ----------------------------------------------------------------
 -- Transactions
 ----------------------------------------------------------------
-
-type Alg = Ed25519_SHA512
-
--- | Single transaction for transfer of coins
-data TxSend = TxSend
-  { txInputs  :: [(Hash Alg, Int)]
-  , txOutputs :: [(PublicKey Alg, Integer)]
-  }
-  deriving (Show, Eq, Generic)
-instance Serialise TxSend
-
-data Tx
-  = Deposit (PublicKey Alg) Integer
-    -- ^ Deposit tokens to given key. Could only appear in genesis
-    --   block
-  | Send (PublicKey Alg) (Signature Alg) TxSend
-    -- ^ Send coins to other addresses. Transaction must obey
-    --   following invariants:
-    --
-    --   0. Signature must be valid
-    --   1. All inputs must be owned by transaction issuer
-    --   3. Inputs and outputs must be nonempty
-    --   2. Sum of inputs must be equal to sum of outputs
-  deriving (Show, Eq, Generic)
-instance Serialise Tx
-
-
-
--- | State of coins in program-digestible format
---
---   Really we'll need to keep in DB to persist it.
-data CoinState = CoinState
-  { unspentOutputs :: Map (Hash Alg, Int) (PublicKey Alg, Integer)
-    -- ^ Map of unspent outputs of transaction. It maps pair of
-    --   transaction hash and output index to amount of coins stored
-    --   there.
-  }
-  deriving (Show)
-
-processDeposit :: Tx -> CoinState -> Maybe CoinState
-processDeposit Send{}                _             = Nothing
-processDeposit tx@(Deposit pk nCoin) CoinState{..} =
-  return CoinState
-    { unspentOutputs = Map.insert (txHash,0) (pk,nCoin) unspentOutputs
-    }
-    where
-      txHash = hashBlob $ toStrict $ serialise tx
-
-
-processTransaction :: Tx -> CoinState -> Maybe CoinState
-processTransaction Deposit{} _ = Nothing
-processTransaction transaction@(Send pubK sig txSend@TxSend{..}) CoinState{..} = do
-  -- Signature must be valid
-  guard $ verifyBlobSignature pubK (toStrict $ serialise txSend) sig
-  -- Inputs and outputs are not null
-  guard $ not $ null txInputs
-  guard $ not $ null txOutputs
-  -- Outputs are all positive
-  forM_ txOutputs $ \(_,n) -> guard (n > 0)
-  -- Inputs are owned Spend and generated amount match and transaction
-  -- issuer have rights to funds
-  inputs <- forM txInputs $ \i -> do
-    (pk,n) <- Map.lookup i unspentOutputs
-    guard $ pk == pubK
-    return n
-  guard (sum inputs == sum (map snd txOutputs))
-  -- Update application state
-  let txHash = hashBlob $ toStrict $ serialise transaction
-  return CoinState
-    { unspentOutputs =
-        let spend txMap = foldl' (flip  Map.delete) txMap txInputs
-            add   txMap = foldl'
-                            (\m (i,out) -> Map.insert (txHash,i) out m)
-                            txMap ([0..] `zip` txOutputs)
-        in add $ spend unspentOutputs
-    }
-
+  
 genesisBlock :: Block Ed25519_SHA512 [Tx]
 genesisBlock = Block
   { blockHeader = Header
@@ -145,22 +70,6 @@ genesisBlock = Block
   , blockData       = [ Deposit (publicKey k) 1000 | PrivValidator k <- toList validatorKeys ]
   , blockLastCommit = Nothing
   }
-
-transitions :: BlockFold CoinState Tx [Tx]
-transitions = BlockFold
-  { processTx           = process
-  , processBlock        = \h txs s0 -> foldM (flip (process h)) s0 txs
-  , transactionsToBlock = \_ ->
-      let selectTx _ []     = []
-          selectTx c (t:tx) = case processTransaction t c of
-                                Nothing -> selectTx c  tx
-                                Just c' -> t : selectTx c' tx
-      in selectTx
-  , initialState        = CoinState Map.empty
-  }
-  where
-    process (Height 0) t s = processDeposit t s <|> processTransaction t s
-    process _          t s = processTransaction t s
 
 ----------------------------------------------------------------
 --
@@ -237,7 +146,7 @@ run validatorSet mPK mWK = do
           --
         , appValidationFun  = \hBlock txs -> do
             coin <- stateAtH coinState hBlock
-            case foldM (flip processTransaction) coin txs of
+            case processBlock transitions hBlock txs coin of
               Nothing -> return False
               Just _  -> return True
           --
