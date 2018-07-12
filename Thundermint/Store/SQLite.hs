bfa30e255d919570c5b6d9d21d5e4334001b1c87
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-07-12 13:30:19 +0300

    Add read-only access to SQLite database

--- Thundermint/Store/SQLite.hs
+++ Thundermint/Store/SQLite.hs
@@ -7,8 +7,9 @@
 -- |
 module Thundermint.Store.SQLite (
     newSQLiteBlockStorage
-  , newSQLiteBlockStorageConn
   , withSQLiteBlockStorage
+  , newSQLiteBlockStorageRO
+  , withSQLiteBlockStorageRO
   ) where
 
 import Codec.Serialise (Serialise,serialise,deserialiseOrFail)
@@ -36,7 +37,8 @@ newSQLiteBlockStorage
   -> IO (BlockStorage 'RW IO alg a)
 newSQLiteBlockStorage dbFile gBlock initalVals = do
   conn <- SQL.open dbFile
-  newSQLiteBlockStorageConn conn gBlock initalVals
+  initializeDatabase conn gBlock initalVals
+  newSQLiteBlockStorageConn conn
 
 -- | Create new block storage using specified file as database
 withSQLiteBlockStorage
@@ -47,50 +49,42 @@ withSQLiteBlockStorage
   -> (BlockStorage 'RW IO alg a -> IO b)
   -> IO b
 withSQLiteBlockStorage dbFile gBlock initalVals action
+  = SQL.withConnection dbFile $ \conn -> do
+      initializeDatabase conn gBlock initalVals
+      action =<< newSQLiteBlockStorageConn conn
+
+-- | Create new block storage using specified file as database
+newSQLiteBlockStorageRO
+  :: (Crypto alg, Serialise a, Serialise (PublicKey alg))
+  => FilePath
+  -> IO (BlockStorage 'RO IO alg a)
+newSQLiteBlockStorageRO dbFile = do
+  conn <- SQL.open dbFile
+  makeReadOnly <$> newSQLiteBlockStorageConn conn
+
+-- | Create new block storage using specified file as database
+withSQLiteBlockStorageRO
+  :: (Crypto alg, Serialise a, Serialise (PublicKey alg))
+  => FilePath
+  -> (BlockStorage 'RO IO alg a -> IO b)
+  -> IO b
+withSQLiteBlockStorageRO dbFile action
   = SQL.withConnection dbFile $ \conn ->
-      action =<< newSQLiteBlockStorageConn conn gBlock initalVals
+      action . makeReadOnly =<< newSQLiteBlockStorageConn conn
+
 
 -- | Create new block storage from connection to SQLite database
 newSQLiteBlockStorageConn
   :: forall alg a. (Crypto alg, Serialise a, Serialise (PublicKey alg))
   => SQL.Connection
-  -> Block alg a
-  -> ValidatorSet alg
   -> IO (BlockStorage 'RW IO alg a)
-newSQLiteBlockStorageConn conn gBlock initalVals = do
+newSQLiteBlockStorageConn conn = do
   mutex <- newMutex
-  -- Initialize tables
-  SQL.execute_ conn
-    "CREATE TABLE IF NOT EXISTS blockchain \
-    \  ( height INT NOT NULL UNIQUE \
-    \  , bid    BLOB NOT NULL \
-    \  , block  BLOB NOT NULL)"
-  SQL.execute_ conn
-    "CREATE TABLE IF NOT EXISTS commits \
-    \  ( height INT  NOT NULL UNIQUE \
-    \  , cmt    BLOB NOT NULL)"
-  SQL.execute_ conn
-    "CREATE TABLE IF NOT EXISTS validators \
-    \  ( height INT  NOT NULL UNIQUE \
-    \  , valset BLOB NOT NULL)"
-  -- Insert genesis block if needed
-  SQL.query_ conn "SELECT MAX(height) FROM blockchain" >>= \case
-    [Only Nothing] -> do
-      SQL.execute conn "INSERT INTO blockchain VALUES (?,?,?)"
-        ( 0 :: Int64
-        , serialise (blockHash gBlock :: BlockID alg a)
-        , serialise gBlock)
-      SQL.execute conn "INSERT INTO validators VALUES (?,?)"
-        ( 1 :: Int64
-        , serialise initalVals
-        )
-    (_::[Only (Maybe Int64)]) -> return ()
   --
   let fetchCommit :: Height -> IO (Maybe (Commit alg a))
       fetchCommit (Height h) = do
         mb <- singleQ conn "SELECT block FROM blockchain WHERE height = ?" (Only (h+1))
         return $ blockLastCommit =<< mb
-
   -- Make dictionary
   return BlockStorage
     { blockchainHeight = withMutex mutex $
@@ -135,6 +129,45 @@ newSQLiteBlockStorageConn conn gBlock initalVals = do
     --
     , closeBlockStorage = SQL.close conn
     }
+
+initializeDatabase
+  :: forall alg a. (Crypto alg, Serialise a, Serialise (PublicKey alg))
+  => SQL.Connection
+  -> Block alg a
+  -> ValidatorSet alg
+  -> IO ()
+initializeDatabase conn gBlock initalVals = do
+  -- Initialize tables
+  SQL.execute_ conn
+    "CREATE TABLE IF NOT EXISTS blockchain \
+    \  ( height INT NOT NULL UNIQUE \
+    \  , bid    BLOB NOT NULL \
+    \  , block  BLOB NOT NULL)"
+  SQL.execute_ conn
+    "CREATE TABLE IF NOT EXISTS commits \
+    \  ( height INT  NOT NULL UNIQUE \
+    \  , cmt    BLOB NOT NULL)"
+  SQL.execute_ conn
+    "CREATE TABLE IF NOT EXISTS validators \
+    \  ( height INT  NOT NULL UNIQUE \
+    \  , valset BLOB NOT NULL)"
+  -- Insert genesis block if needed
+  SQL.query_ conn "SELECT MAX(height) FROM blockchain" >>= \case
+    [Only Nothing] -> do
+      SQL.execute conn "INSERT INTO blockchain VALUES (?,?,?)"
+        ( 0 :: Int64
+        , serialise (blockHash gBlock :: BlockID alg a)
+        , serialise gBlock)
+      SQL.execute conn "INSERT INTO validators VALUES (?,?)"
+        ( 1 :: Int64
+        , serialise initalVals
+        )
+    (_::[Only (Maybe Int64)]) -> return ()
+  
+
+----------------------------------------------------------------
+-- Helpers
+----------------------------------------------------------------
 
 -- Query that returns 0 or 1 result which is CBOR-encoded value
 singleQ :: (SQL.ToRow p, Serialise a)
