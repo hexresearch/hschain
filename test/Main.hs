d1d507eee2f92ecca7894c3850488cc47f95b488
Dmitry Krylov <dima@krylov.ws>
2018-08-07 17:07:30 +1000

    Tests for 'Vote' serialization/deserialization

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,10 @@
 import Test.Tasty
 
 import qualified TM.Network
+import qualified TM.Serialisation
 
 main :: IO ()
 main = defaultMain $ testGroup "test suite"
   [ TM.Network.tests
+  , TM.Serialisation.tests
   ]
