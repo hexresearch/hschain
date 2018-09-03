790328c0ba97344fe82dfbf05a813e9f9c8690fa
Arthur Vardanyan <arthur@datasfer.com>
2018-09-03 19:59:23 +0400

    remove invariant test from mandatory test list.

--- test/Main.hs
+++ test/Main.hs
@@ -3,12 +3,10 @@ import Test.Tasty
 import qualified TM.Network
 import qualified TM.P2P
 import qualified TM.Serialisation
-import qualified TM.Store
 
 main :: IO ()
 main = defaultMain $ testGroup "test suite"
   [ TM.Network.tests
   , TM.P2P.tests
   , TM.Serialisation.tests
-  , TM.Store.tests
   ]
