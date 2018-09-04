c0883fd2c0a662838075725d3566f3100ed44140
Yura Shelyag <yuriy.shelyag@gmail.com>
2018-09-04 23:38:09 +0200

    Fixed getLocalAddress error

--- Thundermint/P2P/Network/TLS.hs
+++ Thundermint/P2P/Network/TLS.hs
@@ -14,6 +14,8 @@ module Thundermint.P2P.Network.TLS (
  , headerSize
   ) where
 
+import Thundermint.P2P.Network  (getLocalAddress)
+
 import Control.Monad            (when)
 import Control.Monad.Catch      (bracketOnError, throwM)
 import Control.Monad.Catch      (MonadMask)
@@ -244,35 +246,6 @@ fill bs0 siz0 recv
           let (bs1,bs2) = BS.splitAt siz bs
           return (LBS.fromStrict (buf `BS.append` bs1), bs2)
 
-
-----------------------------------------------------------------
--- Some useful utilities
-----------------------------------------------------------------
-
--- | Get local node address
---
-getLocalAddress :: IO Net.SockAddr
-getLocalAddress =
-    return $ Net.SockAddrInet 0 (Net.tupleToHostAddress (0x7f, 0, 0, 1))
-    -- TODO get correct `localhost` address
-    --addr:_ <- Net.getAddrInfo (Just $ Net.defaultHints { Net.addrSocketType = Net.Stream })
-    --                          (Just "localhost")
-    --                          (Just "50000")
-    --let sockAddr = Net.addrAddress addr
-    --return sockAddr
-
--------------------------------------------------------------------------------
---
--- | Get local node address
---
-getLocalAddress :: IO Net.SockAddr
-getLocalAddress = do
-    -- TODO get correct `localhost` address
-    addr:_ <- Net.getAddrInfo (Just $ Net.defaultHints { Net.addrSocketType = Net.Stream })
-                              (Just "localhost")
-                              Nothing
-    let sockAddr = Net.addrAddress addr
-    return sockAddr
 
 getCredential :: FilePath -> FilePath -> IO TLS.Credential
 getCredential certFile keyFile = do
