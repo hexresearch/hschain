98cdda95c87beb3532ccda484089085df5982648
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-09-08 22:38:42 +0300

    Add Read instances for crypto types
    These are primarily useful for debugging. Also add tests

--- test/TM/Crypto.hs
+++ test/TM/Crypto.hs
@@ -1,10 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 -- |
 module TM.Crypto (tests) where
 
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Thundermint.Crypto
+import Thundermint.Crypto.Ed25519
+
 tests :: TestTree
 tests = testGroup "Crypto"
-  [
+  [ testCase "read . show = id @ Address/PublicKey/PrivKey Ed25519_SHA512"
+  $ do privK <- generatePrivKey
+       let pubK = publicKey privK
+           addr = address pubK
+       privK @=? (read . show) privK
+       pubK  @=? (read . show) pubK
+       addr  @=? (read . show) addr
+  , testCase "read . show = id @ Address Ed25519_SHA512"
+  $ do let x = hashBlob "ABCD" :: Hash Ed25519_SHA512
+       x @=? (read . show) x
   ]
