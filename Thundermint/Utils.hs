955c773e652b1018824f23070d0bea37a772c9b4
Dmitry Krylov <dima@krylov.ws>
2018-09-13 16:19:03 +1000

    Serialize/deserialize PeerId for handshake

---
+++ Thundermint/Utils.hs
@@ -0,0 +1,55 @@
+-- | Some small but useful functions
+--
+{-# LANGUAGE CPP #-}
+module Thundermint.Utils
+   ( -- * PeerId encode/decode
+     PeerId
+   , peerIdLength
+   , encodePeerId
+   , decodePeerId
+   ) where
+
+
+#include "MachDeps.h"
+
+
+import Data.Word
+import Foreign.ForeignPtr
+import Foreign.Ptr
+import Foreign.Storable
+import System.IO.Unsafe
+import qualified Data.ByteString.Internal as B
+import qualified Data.ByteString.Lazy as BL
+
+
+type PeerId = Word64
+
+
+peerIdLength :: Int
+peerIdLength = 8
+
+
+#ifdef WORDS_BIGENDIAN
+
+    Sorry! decodePeerId & encodePeerId not implemented on your arch yet!
+
+#else
+
+
+encodePeerId :: PeerId -> BL.ByteString
+encodePeerId m = BL.fromStrict $ B.unsafeCreate peerIdLength (\p -> poke (castPtr p) m)
+
+
+decodePeerId :: BL.ByteString -> Either String PeerId
+decodePeerId bs =
+    let (fp', ofs, len) = B.toForeignPtr (BL.toStrict bs)
+        fp = plusForeignPtr fp' ofs
+    in
+    if ofs == 0 then
+        if len == peerIdLength
+        then unsafePerformIO $ withForeignPtr fp (fmap Right . peek . castPtr)
+        else Left ("Wrong PeerId length (expected = " ++ show peerIdLength ++ ", actual = " ++ show len ++ ")")
+    else
+        Left ("Wrong ofs = " ++ show ofs)
+
+#endif
