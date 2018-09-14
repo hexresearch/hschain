10d389bf6ba64626d79099d08878fca46452ac60
Dmitry Krylov <dima@krylov.ws>
2018-09-14 17:22:31 +1000

    Update & resort Nix dependencies

--- release.nix
+++ release.nix
@@ -7,7 +7,7 @@ let
       docker-container = pkgs.dockerTools.buildImage {
         name = "thundermint-node";
         fromImageName = "scratch";
-        contents = [haskellPackages.thundermint pkgs.bashInteractive];
+        contents = [haskellPackages.thundermint];
         config = {
           Volumes = {
             "/thundermint" = {};
@@ -20,27 +20,28 @@ let
         };
       haskellPackages = pkgs.haskellPackages.override {
         overrides = haskellPackagesNew: haskellPackagesOld: rec {
-          lifted-async =  haskellPackagesNew.lifted-async_0_10_0_1;
-          async = haskellPackagesNew.async_2_2_1;
           aeson = haskellPackagesNew.aeson_1_3_0_0;
-          text = haskellPackagesNew.text_1_2_3_0;
-          tasty-hunit = lib.dontCheck haskellPackagesNew.tasty-hunit_0_10_0_1;
-          tasty = lib.dontCheck haskellPackagesOld.tasty;
-          serialise = lib.dontCheck haskellPackagesOld.serialise;
-          network = haskellPackagesOld.callPackage ./deps/network.nix {};
-          network-simple = haskellPackagesOld.callPackage ./deps/network-simple.nix {};
-          exceptions = haskellPackagesOld.callPackage ./deps/exceptions.nix {};
-          safe-exceptions = haskellPackagesOld.callPackage ./deps/safe-exceptions.nix {};
-          katip = haskellPackagesOld.callPackage ./deps/katip.nix {};
-          free = haskellPackagesOld.callPackage ./deps/free.nix {};
-          either = haskellPackagesOld.callPackage ./deps/either.nix {};
-          concurrent-output = haskellPackagesOld.callPackage ./deps/concurrent-output.nix {};
-          hedgehog = haskellPackagesOld.callPackage ./deps/hedgehog.nix {};
-          memory = haskellPackagesOld.callPackage ./deps/memory.nix {};
-          cryptonite = lib.dontCheck( haskellPackagesOld.callPackage ./deps/cryptonite.nix {} );
+          async = haskellPackagesNew.async_2_2_1;
           basement = haskellPackagesOld.callPackage ./deps/basement.nix {};
+          concurrent-output = haskellPackagesOld.callPackage ./deps/concurrent-output.nix {};
+          cryptonite = lib.dontCheck( haskellPackagesOld.callPackage ./deps/cryptonite.nix {} );
+          either = haskellPackagesOld.callPackage ./deps/either.nix {};
+          exceptions = haskellPackagesOld.callPackage ./deps/exceptions.nix {};
           foundation = haskellPackagesOld.callPackage ./deps/foundation.nix {};
+          free = haskellPackagesOld.callPackage ./deps/free.nix {};
+          hedgehog = haskellPackagesOld.callPackage ./deps/hedgehog.nix {};
+          katip = haskellPackagesOld.callPackage ./deps/katip.nix {};
+          lifted-async =  haskellPackagesNew.lifted-async_0_10_0_1;
+          memory = haskellPackagesOld.callPackage ./deps/memory.nix {};
+          network = haskellPackagesOld.callPackage ./deps/network.nix {};
+          network-info = haskellPackagesOld.callPackage ./deps/network-info.nix {};
+          network-simple = haskellPackagesOld.callPackage ./deps/network-simple.nix {};
           retry = haskellPackagesOld.callPackage ./deps/retry.nix {};
+          safe-exceptions = haskellPackagesOld.callPackage ./deps/safe-exceptions.nix {};
+          serialise = lib.dontCheck haskellPackagesOld.serialise;
+          tasty = lib.dontCheck haskellPackagesOld.tasty;
+          tasty-hunit = lib.dontCheck haskellPackagesNew.tasty-hunit_0_10_0_1;
+          text = haskellPackagesNew.text_1_2_3_0;
           thundermint = pkgs.haskell.lib.overrideCabal
               ( pkgs.haskell.lib.justStaticExecutables
                   ( lib.dontCheck(haskellPackagesNew.callPackage ./thundermint.nix { }))
