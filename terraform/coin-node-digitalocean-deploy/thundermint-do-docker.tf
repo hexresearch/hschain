b6a46f2e8e6a4072c9a57cf94a0d40507337502a
romcheck <romcheck@gmail.com>
2018-10-01 01:22:49 +0200

    upd inventory, deploy restarting thundermint coin nodes

--- terraform/coin-node-digitalocean-deploy/thundermint-do-docker.tf
+++ terraform/coin-node-digitalocean-deploy/thundermint-do-docker.tf
@@ -1,5 +1,4 @@
 variable "dotoken" {}
-variable "elasticpass" {}
 
 provider "digitalocean" {
   token = "${var.dotoken}"
@@ -20,38 +19,25 @@ variable "regions" {
 
 resource "digitalocean_droplet" "node" {
 
-  count    = "${length(var.private_keys)}"
-  image    = "docker-18-04"
-  size     = "4gb"
-  region   = "${element(var.regions, count.index)}"
-  name     = "node-${count.index+1}"
+  count = "${length(var.private_keys)}"
+  image = "docker-18-04"
+  size  = "4gb"
+  region = "${element(var.regions, count.index)}"
+  name  = "node-${count.index+1}"
   ssh_keys = ["22607679", "22838852", "22986074"]
-
-  provisioner "file" {
-
-    content = "input { gelf {} } output { elasticsearch { hosts => ['https://elastic.hxr.team:443'] user => 'elastic' password => '${var.elasticpass}' } }"
-    destination = "/opt/logstash.conf"
-
-    connection {
-      type    = "ssh"
-      timeout = "1m"
-    }
-
-  }
 
   provisioner "remote-exec" {
 
     connection {
-      type         = "ssh"
-      timeout      = "1m"
-      #private_key = "${file("~/.ssh/id_rsa")}"
-      #agent       = false
+      type = "ssh"
+      timeout = "1m"
+      private_key = "${file("~/.ssh/id_rsa")}"
+      agent = false
     }
 
     inline = [
-      "docker run -p 172.17.0.1:12201:12201/udp -d -v /opt/logstash.conf:/logstash.conf docker.elastic.co/logstash/logstash-oss:6.4.2 -f /logstash.conf",
       "docker volume create thundermint",
-      "docker run --log-driver gelf --log-opt gelf-address=udp://172.17.0.1:12201 -v thundermint:/thundermint -p 49999-50000:49999-50000 --name node-${count.index+1} -d -e THUNDERMINT_KEYS='${jsonencode(values(var.private_keys))}' -e THUNDERMINT_NODE_SPEC='{ \"nspecPrivKey\":\"${var.private_keys[count.index]}\", \"nspecDbName\": \"db/node-${count.index+1}\", \"nspecLogFile\" : [{ \"type\": \"ScribeJSON\", \"severity\" : \"Debug\", \"verbosity\" : \"V2\" }], \"nspecWalletKeys\"  : [${count.index*500},500]}' dmalkr/thundermint-node /bin/thundermint-coin-node --prefix /thundermint --delay 100 --check-consensus --deposit 1000 --keys 2000"
+      "docker run -v thundermint:/thundermint -p 49999-50000:49999-50000 --name node-${count.index+1} -d -e THUNDERMINT_KEYS='${jsonencode(values(var.private_keys))}' -e THUNDERMINT_NODE_SPEC='{ \"nspecPrivKey\":\"${var.private_keys[count.index]}\", \"nspecDbName\": \"db/node-${count.index+1}\", \"nspecLogFile\" : [{ \"type\": \"ScribeJSON\", \"path\" : \"logs/node-${count.index+1}\", \"severity\" : \"Debug\", \"verbosity\" : \"V2\" }], \"nspecWalletKeys\"  : [${count.index*500},500]}' dmalkr/thundermint-node /bin/thundermint-coin-node --prefix /thundermint --delay 100 --check-consensus --deposit 1000 --keys 2000"
     ]
 
   }
