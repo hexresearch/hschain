f33d40ace4eba1ef47b934751d5e8ba290bc410a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-10-01 19:36:08 +0300

    Add tests for mempool
    They fail at the moment

--- test/Main.hs
+++ test/Main.hs
@@ -4,6 +4,7 @@ import Test.Tasty
 import qualified TM.Network
 import qualified TM.NetworkTls
 import qualified TM.P2P
+import qualified TM.Mempool
 import qualified TM.Serialisation
 import qualified TM.Store
 import qualified TM.Crypto
@@ -14,6 +15,7 @@ main = defaultMain $ testGroup "test suite"
   [ TM.Network.tests
   , TM.P2P.tests
   , TM.NetworkTls.tests
+  , TM.Mempool.tests
   , TM.Serialisation.tests
   , TM.Store.tests
   , TM.Crypto.tests
