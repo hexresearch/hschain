39a10db38a3be6b27b55813c8e1cbb5dadea1cd9
Dmitry Krylov <dima@krylov.ws>
2018-10-08 23:22:29 +1000

    Startup test script

--- start-coin-node-cluster-locally.sh
+++ start-coin-node-cluster-locally.sh
@@ -8,59 +8,51 @@ export THUNDERMINT_KEYS="[\"2K7bFuJXxKf5LqogvVRQjms2W26ZrjpvUjo5LdvPFa5Y\",\"4NS
 
 PREFIX=tmp/thundermint
 
-COMMON_OPTIONS="--max-h 100 --delay 100 --check-consensus --deposit 1000 --keys 2000 +RTS -hr -L75 -Pa"
+COMMON_OPTIONS="--max-h 100 --delay 100 --check-consensus --deposit 1000 --keys 2000"
+
+LOG_SPEC="\"nspecLogFile\" : [{ \"type\": \"ScribeES\", \"path\" : \"`cat elastic-search.cfg`\", \"severity\" : \"Debug\", \"verbosity\" : \"V2\" }]"
+# LOG_SPEC="\"nspecLogFile\" : [{ \"type\": \"ScribeTXT\", \"severity\" : \"Debug\", \"verbosity\" : \"V2\" }]"
 
 rm -rf $PREFIX
-mkdir -p $PREFIX
-mkdir $PREFIX/node-1
-mkdir $PREFIX/node-2
-mkdir $PREFIX/node-3
-mkdir $PREFIX/node-4
+mkdir --parents $PREFIX
+mkdir --parents $PREFIX/node-1/db
+mkdir --parents $PREFIX/node-2/db
+mkdir --parents $PREFIX/node-3/db
+mkdir --parents $PREFIX/node-4/db
 
 trap 'kill 0' SIGINT
 
+bootstrap() {
+    bs=$1
+    ( echo -e $bs | nc 127.0.0.1 49999 -w 1 ) || ( sleep 0.1 ; bootstrap $bs > /dev/null 2> /dev/null )
+}
+
 echo "Node 1"
-export THUNDERMINT_NODE_SPEC="{ \"nspecPrivKey\":\"2K7bFuJXxKf5LqogvVRQjms2W26ZrjpvUjo5LdvPFa5Y\", \"nspecDbName\": \"db/node-1\", \"nspecLogFile\" : [{ \"type\": \"ScribeTXT\", \"path\" : \"logs/node-1\", \"severity\" : \"Debug\", \"verbosity\" : \"V2\" }], \"nspecWalletKeys\"  : [0,1]}"
-pushd $PREFIX/node-1 > /dev/null
-stack exec thundermint-coin-node -- --listen-port 50001 $COMMON_OPTIONS &
-pid1=$!
-popd > /dev/null
-sleep 0.5
-echo -e "[\"127.0.0.1:50002\",\"127.0.0.1:50003\",\"127.0.0.1:50004\"]\n"  | nc 127.0.0.1 49999 -w 1
+export THUNDERMINT_NODE_SPEC="{ \"nspecPrivKey\":\"2K7bFuJXxKf5LqogvVRQjms2W26ZrjpvUjo5LdvPFa5Y\", \"nspecDbName\": \"./db/node-1\", $LOG_SPEC, \"nspecWalletKeys\"  : [0,1]}"
+( cd $PREFIX/node-1 ; stack exec thundermint-coin-node -- --listen-port 50001 $COMMON_OPTIONS & )
+bootstrap "[\"127.0.0.1:50002\",\"127.0.0.1:50003\",\"127.0.0.1:50004\"]\n"
 sleep 0.5
 
 echo "Node 2"
-export THUNDERMINT_NODE_SPEC="{ \"nspecPrivKey\":\"4NSWtMsEPgfTK25tCPWqNzVVze1dgMwcUFwS5WkSpjJL\", \"nspecDbName\": \"db/node-2\", \"nspecLogFile\" : [{ \"type\": \"ScribeTXT\", \"path\" : \"logs/node-2\", \"severity\" : \"Debug\", \"verbosity\" : \"V2\" }], \"nspecWalletKeys\"  : [1,1]}"
-pushd $PREFIX/node-2 > /dev/null
-stack exec thundermint-coin-node -- --listen-port 50002 $COMMON_OPTIONS &
-pid2=$!
-popd > /dev/null
-sleep 0.5
-echo -e "[\"127.0.0.1:50003\",\"127.0.0.1:50004\"]\n"  | nc 127.0.0.1 49999 -w 1
+export THUNDERMINT_NODE_SPEC="{ \"nspecPrivKey\":\"4NSWtMsEPgfTK25tCPWqNzVVze1dgMwcUFwS5WkSpjJL\", \"nspecDbName\": \"./db/node-2\", $LOG_SPEC, \"nspecWalletKeys\"  : [1,1]}"
+( cd $PREFIX/node-2 ; stack exec thundermint-coin-node -- --listen-port 50002 $COMMON_OPTIONS & )
+bootstrap "[\"127.0.0.1:50003\",\"127.0.0.1:50004\"]\n"
 sleep 0.5
 
 echo "Node 3"
-export THUNDERMINT_NODE_SPEC="{ \"nspecPrivKey\":\"3Fj8bZjKc53F2a87sQaFkrDas2d9gjzK57FmQwnNnSHS\", \"nspecDbName\": \"db/node-3\", \"nspecLogFile\" : [{ \"type\": \"ScribeTXT\", \"path\" : \"logs/node-3\", \"severity\" : \"Debug\", \"verbosity\" : \"V2\" }], \"nspecWalletKeys\"  : [2,1]}"
-pushd $PREFIX/node-3 > /dev/null
-stack exec thundermint-coin-node -- --listen-port 50003 $COMMON_OPTIONS &
-pid3=$!
-popd > /dev/null
-sleep 0.5
-echo -e "[\"127.0.0.1:50004\"]\n"  | nc 127.0.0.1 49999 -w 1
+export THUNDERMINT_NODE_SPEC="{ \"nspecPrivKey\":\"3Fj8bZjKc53F2a87sQaFkrDas2d9gjzK57FmQwnNnSHS\", \"nspecDbName\": \"./db/node-3\", $LOG_SPEC, \"nspecWalletKeys\"  : [2,1]}"
+( cd $PREFIX/node-3 ; stack exec thundermint-coin-node -- --listen-port 50003 $COMMON_OPTIONS & )
+bootstrap "[\"127.0.0.1:50004\"]\n"
 sleep 0.5
 
 echo "Node 4"
-export THUNDERMINT_NODE_SPEC="{ \"nspecPrivKey\":\"D2fpHM1JA8trshiUW8XPvspsapUvPqVzSofaK1MGRySd\", \"nspecDbName\": \"db/node-4\", \"nspecLogFile\" : [{ \"type\": \"ScribeTXT\", \"path\" : \"logs/node-4\", \"severity\" : \"Debug\", \"verbosity\" : \"V2\" }], \"nspecWalletKeys\"  : [3,1]}"
-pushd $PREFIX/node-4 > /dev/null
-stack exec thundermint-coin-node -- --listen-port 50004 $COMMON_OPTIONS &
-pid4=$!
-popd > /dev/null
-sleep 0.5
-echo -e "[]\n"  | nc 127.0.0.1 49999 -w 1
+export THUNDERMINT_NODE_SPEC="{ \"nspecPrivKey\":\"D2fpHM1JA8trshiUW8XPvspsapUvPqVzSofaK1MGRySd\", \"nspecDbName\": \"./db/node-4\", $LOG_SPEC, \"nspecWalletKeys\"  : [3,1]}"
+( cd $PREFIX/node-4 ; stack exec thundermint-coin-node -- --listen-port 50004 $COMMON_OPTIONS & )
+bootstrap "[]\n"
 sleep 0.5
 
 echo "Ok, node started!"
 echo "Waiting for node finished"
-wait
+sleep 1h
 
 echo "OK"
