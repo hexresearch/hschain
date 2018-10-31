e2d963901f96a82b929017a56c1043f7d48f4cb7
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-10-31 15:42:57 +0300

    Move all transaction generation infrastructure to Mock.Coin

--- thundermint/exe/coin-node.hs
+++ thundermint/exe/coin-node.hs
@@ -6,14 +6,14 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE TypeFamilies        #-}
-import Control.Concurrent
+import Codec.Serialise  (Serialise)
 import Control.Monad.Catch
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Reader
 import Data.Int
 import Options.Applicative
 
-import Control.Monad        (forever, void)
+import Control.Monad        (void)
 import Katip.Core           (showLS)
 import Network.Simple.TCP   (accept, listen, recv, closeSock)
 import Network.Socket       (SockAddr(..), PortNumber)
@@ -23,16 +23,14 @@ import System.FilePath      ((</>))
 import Thundermint.Blockchain.Internal.Engine.Types
 import Thundermint.Blockchain.Types
 import Thundermint.Control
-import Thundermint.Crypto
 import Thundermint.Crypto.Containers
 import Thundermint.Crypto.Ed25519            (Ed25519_SHA512)
+import Thundermint.Debug.Trace
 import Thundermint.Logger
 import Thundermint.Run
 import Thundermint.Store
-import Thundermint.Store.SQL            (runEphemeralQ)
 import Thundermint.Store.Internal.Query (Connection, connectionRO)
 import Thundermint.Mock.Coin
-import Thundermint.Mock.KeyList
 import Thundermint.Mock.Types
 import Thundermint.P2P.Consts
 import Thundermint.P2P.Instances ()
@@ -54,6 +52,8 @@ data Opts = Opts
   , doValidate        :: Bool
   , netInitialDeposit :: Integer
   , netInitialKeys    :: Int
+  , nodeNumber        :: Int
+  , totalNodes        :: Int
   , optTls            :: Bool
   }
 
@@ -63,15 +63,19 @@ data Opts = Opts
 --
 ----------------------------------------------------------------
 
+-- | Interpret specification for node
 interpretSpec
-  :: Opts
-  -> ValidatorSet Ed25519_SHA512
-  -> BlockchainNet SockAddr
-  -> NodeSpec
-  -> LoggerT IO (Connection 'RO Alg [Tx], LoggerT IO ())
-interpretSpec Opts{..} validatorSet net NodeSpec{..} = do
+  :: ( MonadIO m, MonadLogger m, MonadFork m, MonadTrace m, MonadMask m
+     , Ord addr, Show addr, Serialise addr)
+  => Maybe Int64                      -- ^ Maximum height
+  -> GeneratorSpec                    -- ^ Spec for generator of transactions
+  -> ValidatorSet Ed25519_SHA512      -- ^ Set of validators
+  -> BlockchainNet addr               -- ^ Network
+  -> NodeSpec                         -- ^ Node specifications
+  -> m (Connection 'RO Alg [Tx], m ())
+interpretSpec maxHeight genSpec validatorSet net NodeSpec{..} = do
   -- Allocate storage for node
-  conn   <- openDatabase (maybe ":memory:" (prefix </>) nspecDbName)
+  conn   <- openDatabase (maybe ":memory:" id nspecDbName)
             coinDict genesisBlock validatorSet
   return
     ( connectionRO conn
@@ -79,21 +83,12 @@ interpretSpec Opts{..} validatorSet net NodeSpec{..} = do
         logic  <- logicFromPersistent transitionsDB
         -- Transactions generator
         cursor <- getMempoolCursor $ nodeMempool logic
-        let generator = forever $ do
-              let (off,n)  = nspecWalletKeys
-                  privKeys = take n $ drop off privateKeyList
-              txGen   <- liftIO
-                       $ generateTransaction (publicKey <$> take netInitialKeys privateKeyList) privKeys
-              Just tx <- queryRO
-                       $ runEphemeralQ coinDict
-                       $ txGen
-              void $ pushTransaction cursor tx
-              liftIO $ threadDelay (delay * 1000)
+        let generator = transactionGenerator genSpec (void . pushTransaction cursor)
         acts <- runNode defCfg net
           NodeDescription
             { nodeValidationKey   = nspecPrivKey
             , nodeCommitCallback  = \case
-                h | Just hM <- maxH
+                h | Just hM <- maxHeight
                   , h > Height hM -> throwM Abort
                   | otherwise     -> return ()
             }
@@ -102,19 +97,8 @@ interpretSpec Opts{..} validatorSet net NodeSpec{..} = do
     )
   where
     genesisBlock :: Block Alg [Tx]
-    genesisBlock =  Block
-      { blockHeader = Header
-          { headerChainID        = "MONIES"
-          , headerHeight         = Height 0
-          , headerTime           = Time 0
-          , headerLastBlockID    = Nothing
-          , headerValidatorsHash = hash validatorSet
-          }
-      , blockData       = [ Deposit (publicKey pk) netInitialDeposit
-                          | pk <- take netInitialKeys privateKeyList
-                          ]
-      , blockLastCommit = Nothing
-      }
+    genesisBlock = genesisFromGenerator validatorSet genSpec
+
 
 ----------------------------------------------------------------
 --
@@ -122,12 +106,12 @@ interpretSpec Opts{..} validatorSet net NodeSpec{..} = do
 
 main :: IO ()
 main = do
-    opts@Opts{..} <- customExecParser (prefs showHelpOnError)
-                  $ info (helper <*> parser)
-                    (  fullDesc
-                    <> header   "Coin test program"
-                    <> progDesc ""
-                    )
+    Opts{..} <- customExecParser (prefs showHelpOnError)
+              $ info (helper <*> parser)
+                (  fullDesc
+                <> header   "Coin test program"
+                <> progDesc ""
+                )
     nodeSpecStr <- BC8.pack <$> getEnv "THUNDERMINT_NODE_SPEC"
     ipMapPath   <- BC8.pack <$> getEnv "THUNDERMINT_KEYS"
     let nodeSpec@NodeSpec{..} = either error id $ JSON.eitherDecodeStrict' nodeSpecStr
@@ -155,7 +139,9 @@ main = do
                    , bchLocalAddr        = nodeAddr
                    , bchInitialPeers     = netAddresses
                    }
-      (_,act) <- interpretSpec opts validatorSet net nodeSpec
+          genSpec = restrictGenerator nodeNumber totalNodes
+                  $ defaultGenerator netInitialKeys netInitialDeposit delay
+      (_,act) <- interpretSpec maxH genSpec validatorSet net nodeSpec
       act `catch` (\Abort -> return ())
   where
     parser :: Parser Opts
@@ -194,6 +180,16 @@ main = do
       netInitialKeys <- option auto
         (  long "keys"
         <> help "Initial deposit"
+        <> metavar "N"
+        )
+      nodeNumber <- option auto
+        (  long "node-n"
+        <> help "Node number"
+        <> metavar "N"
+        )
+      totalNodes <- option auto
+        (  long "total-nodes"
+        <> help "Node number"
         <> metavar "N"
         )
       optTls <- switch
