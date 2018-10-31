3654c69019e60a20412b74901cb9a42cf93bc4c2
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-10-31 17:32:16 +0300

    Add coin to tests;

--- thundermint/test/TM/Store.hs
+++ thundermint/test/TM/Store.hs
@@ -11,10 +11,12 @@ import Data.Traversable (forM)
 import qualified Data.Aeson            as JSON
 import qualified Data.ByteString.Char8 as BC8
 
+import           Thundermint.Blockchain.Types
 import           Thundermint.Run
 import           Thundermint.Store
+import           Thundermint.Store.SQL
 import qualified Thundermint.Mock.KeyVal as KeyVal
-
+import qualified Thundermint.Mock.Coin   as Coin
 
 maxHeight :: Int64
 maxHeight = 10
@@ -23,6 +25,7 @@ tests :: TestTree
 tests = testGroup "generate blockchain and check on consistency"
   [ testGroup "blockhains"
     [ testCase "key-val db" $ runKeyVal (Just maxHeight)  "./test-data/key-val" "../spec/simple-stm.json"
+    , testCase "Mock coin"  $ runCoin   (Just maxHeight) "../spec/keyval-stm.json"
     ]
   ]
 
@@ -38,3 +41,47 @@ runKeyVal maxH prefix file = do
       -- Check result against consistency invariants
       checks <- forM storageList $ \c -> runDBT c checkStorage
       assertEqual "failed consistency check" [] (concat checks)
+
+
+-- Run coin blockchain mock
+runCoin :: Maybe Int64 -> FilePath -> IO ()
+runCoin maxH file = do
+  -- read config
+  blob <- BC8.readFile file
+  spec <- case JSON.eitherDecodeStrict blob of
+    Right s -> return s
+    Left  e -> error e
+  storageList <- Coin.executeNodeSpec maxH 300 spec
+  -- Check that each blockchain is internally consistent\
+  checks <- forM storageList $ \c -> runDBT c checkStorage
+  assertEqual "failed consistency check" [] (concat checks)
+  -- Check that block and validators identical on each node
+  let Just maximumH = maxH
+      heights = map (Height . fromIntegral) [0 .. maximumH]
+  coins <- forM heights $ \h -> do
+    -- blocks
+    blocks <- forM storageList $ \c -> do
+      mb <- runDBT c $ queryRO $ retrieveBlock h
+      case mb of
+        Nothing -> error ("Missing block at " <> show h)
+        Just b  -> return b
+    assertBool ("Block mismatch!" <> show h <> "\n" <> show blocks) (allEqual blocks)
+    -- Check that validator set match
+    vals <- forM storageList $ \c -> do
+      mv <- runDBT c $ queryRO $ retrieveValidatorSet h
+      case (h,mv) of
+        (Height 0, Nothing) -> return Nothing
+        (_       , Just v ) -> return (Just v)
+        _                   -> error "Invalid validator!"
+    assertBool ("Validators mismatch!" <> show h) (allEqual vals)
+    --
+    utxos <- forM storageList $ \c ->
+      runDBT c $ queryRO $ queryUserState h Coin.coinDict $ materializePMap Coin.unspentOutputsLens
+    let amount = sum . fmap snd <$> utxos
+    assertBool ("Coin amount mismatch") (allEqual amount)
+  assertBool ("Coin amount changed: " ++ show coins) (allEqual coins)
+
+
+allEqual :: Eq a => [a] -> Bool
+allEqual []     = error "Empty list impossible!"
+allEqual (x:xs) = all (x==) xs
