591ce72cc6fa21608cc98ac4765b8436910415ff
romcheck <romcheck@gmail.com>
2019-01-20 13:44:02 +0900

    fix publish image, upd readme ipv6 note

--- dockerize-ci.sh
+++ dockerize-ci.sh
@@ -6,7 +6,6 @@ REGISTRY=registry.hxr.team
 branch=$DRONE_BRANCH
 gittag=$DRONE_TAG
 event=$DRONE_BUILD_EVENT
-prnumber=$DRONE_PULL_REQUEST
 publish=false
 
 if [[ -n "${gittag// }" ]]; then
@@ -15,9 +14,6 @@ if [[ -n "${gittag// }" ]]; then
 else
     if [[ "$branch" == "master" && "$event" != "pull_request" ]]; then
         tag="latest"
-        publish=true
-    elif [[ != "pull_request" ]];
-        tag="$prnumber"
         publish=true
     else
         tag="latest"
