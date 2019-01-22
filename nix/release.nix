fba70b9434a575e47fc36474f6ee91402a2e04b1
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-01-22 01:40:30 +0000

    Override packages for all GHC versions

--- nix/release.nix
+++ nix/release.nix
@@ -1,13 +1,27 @@
 let
-
-  lib = pkgs.haskell.lib;
-  config  = {
+  pkgs   = import ./pkgs.nix { inherit config; };
+  lib    = pkgs.haskell.lib;
+  haskOverrides = hsNew: hsOld: rec {
+    concurrent-output   = hsOld.callPackage ./derivations/concurrent-output.nix {};
+    hedgehog            = hsOld.callPackage ./derivations/hedgehog.nix {};
+    katip               = hsOld.callPackage ./derivations/katip.nix {};
+    katip-elasticsearch = lib.dontCheck (hsOld.callPackage ./derivations/katip-elasticsearch.nix {});
+    prometheus-client   = hsOld.callPackage ./derivations/prometheus-client.nix {};
+    stm                 = hsOld.callPackage ./derivations/stm.nix {};
+    # -
+    thundermint-crypto  = hsOld.callPackage ./derivations/thundermint-crypto.nix {};
+    thundermint-types   = hsOld.callPackage ./derivations/thundermint-types.nix {};
+    thundermint         = hsOld.callPackage ./derivations/thundermint.nix {};
+    thundermint-exe     = pkgs.haskell.lib.overrideCabal
+      (lib.justStaticExecutables (lib.dontCheck thundermint)) (oldDerivation: { });
+  };
+  config = {
     allowUnfree = true;
     packageOverrides = pkgs: rec {
       docker-container = pkgs.dockerTools.buildImage {
         name = "thundermint-node";
         fromImageName = "scratch";
-        contents = [haskellPackages.thundermint-exe];
+        contents = [pkgs.haskellPackages.thundermint-exe];
         config = {
           Volumes = {
             "/thundermint" = {};
@@ -17,32 +31,13 @@ let
             "50000" = {};
           };
           };
-        };
-      haskellPackages = pkgs.haskellPackages.override {
-        overrides = hsNew: hsOld: rec {
-          concurrent-output   = hsOld.callPackage ./derivations/concurrent-output.nix {};
-          hedgehog            = hsOld.callPackage ./derivations/hedgehog.nix {};
-          katip               = hsOld.callPackage ./derivations/katip.nix {};
-          katip-elasticsearch = lib.dontCheck (hsOld.callPackage ./derivations/katip-elasticsearch.nix {});
-          prometheus-client   = hsOld.callPackage ./derivations/prometheus-client.nix {};
-          stm                 = hsOld.callPackage ./derivations/stm.nix {};
-          # -
-          thundermint-crypto  = hsOld.callPackage ./derivations/thundermint-crypto.nix {};
-          thundermint-types   = hsOld.callPackage ./derivations/thundermint-types.nix {};
-          thundermint         = hsOld.callPackage ./derivations/thundermint.nix {};
-          thundermint-exe     = pkgs.haskell.lib.overrideCabal
-              ( pkgs.haskell.lib.justStaticExecutables
-                ( lib.dontCheck thundermint))( oldDerivation: { }
-              );
-        };
+      };
+      haskell = pkgs.haskell // {
+        packageOverrides = haskOverrides;
       };
     };
   };
-
-  pkgs = import ./pkgs.nix { inherit config; };
-
-  callPackage = pkgs.haskellPackages.callPackage;
-
+  # ----------------------------------------
   self = rec {
     inherit pkgs;
     thundermint         = pkgs.haskellPackages.thundermint;
