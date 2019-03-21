690df7c6bff61550a0b46ec300b3c1c3a0390b72
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-03-21 16:55:19 +0300

    Allow use of nix-shell in project root

--- shell.nix
+++ shell.nix
@@ -1,16 +1,1 @@
-args@{
-  isProd      ? false
-, isProfile   ? false
-, isGhc86     ? false
-, isGhcjs     ? false
-}:
-let release = import ./nix/release.nix args;
-    pkgs = release.pkgs;
-    drvs = if isGhcjs then release.packagesGHCJS else release.thundermintPackages;
-in release.pkgs.haskellPackages.shellFor {
-    nativeBuildInputs = with pkgs.haskellPackages; [
-      cabal-install
-      ghcid
-    ];
-    packages = _: pkgs.lib.attrValues drvs;
-  }
+import nix/shell.nix
