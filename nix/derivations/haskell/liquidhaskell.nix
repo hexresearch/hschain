f37cedda26b086a6748e8a19134ed844f381c8f9
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-04-21 20:38:02 +0300

    Update liquid haskell

--- nix/derivations/haskell/liquidhaskell.nix
+++ nix/derivations/haskell/liquidhaskell.nix
@@ -1,19 +1,19 @@
 { mkDerivation, aeson, base, binary, bytestring, cereal, cmdargs
 , containers, data-default, deepseq, Diff, directory, fetchgit
-, filepath, fingertree, ghc, ghc-boot, ghc-paths, ghc-prim, gitrev
-, hashable, hscolour, liquid-fixpoint, mtl, optparse-applicative
-, optparse-simple, parsec, pretty, process, stdenv, stm, syb
-, tagged, tasty, tasty-ant-xml, tasty-hunit, tasty-rerun
-, template-haskell, temporary, text, time, transformers
-, unordered-containers, vector, z3
+, filepath, fingertree, ghc, ghc-boot, ghc-paths, ghc-prim, githash
+, gitrev, hashable, hscolour, liquid-fixpoint, mtl
+, optparse-applicative, optparse-simple, parsec, pretty, process
+, stdenv, stm, syb, tagged, tasty, tasty-ant-xml, tasty-hunit
+, tasty-rerun, template-haskell, temporary, text, time
+, transformers, unordered-containers, vector, z3
 }:
 mkDerivation {
   pname = "liquidhaskell";
   version = "0.8.6.0";
   src = fetchgit {
     url = "https://github.com/ucsd-progsys/liquidhaskell.git";
-    sha256 = "0h5a7p8gac1v2lrnl12w50njlgqqgbnbp5nqrlr1q1vw409q5yxf";
-    rev = "580aef36671058c9f8a28bf5900acf41d70b4f89";
+    sha256 = "02r4hxzjk96765flx5la0bk803rm5ypxi54cgf6xisqikx68h5kd";
+    rev = "e725abded15fc8f7cbedd66781ad56345318f6b4";
     fetchSubmodules = true;
   };
   isLibrary = true;
@@ -22,7 +22,7 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson base binary bytestring cereal cmdargs containers data-default
     deepseq Diff directory filepath fingertree ghc ghc-boot ghc-paths
-    ghc-prim gitrev hashable hscolour liquid-fixpoint mtl
+    ghc-prim githash gitrev hashable hscolour liquid-fixpoint mtl
     optparse-simple parsec pretty syb template-haskell temporary text
     time transformers unordered-containers vector
   ];
