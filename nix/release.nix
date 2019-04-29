4fb7a8149335b534f2c3567fdeaba017ad8b45ae
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-04-29 12:44:11 +0300

    Use ignoresources from hask tools

--- nix/release.nix
+++ nix/release.nix
@@ -37,18 +37,9 @@ let
     hask.doPedantic (lib.doCheck drv));
   profileOverride = doIf isProfile hask.doProfile;
   #
-  ignoreStack = source:
-    let
-      gitignore = pkgs.callPackage (pkgs.fetchFromGitHub {
-        owner = "siers";
-        repo = "nix-gitignore";
-        rev = "ce0778ddd8b1f5f92d26480c21706b51b1af9166";
-        sha256 = "1d7ab78i2k13lffskb23x8b5h24x7wkdmpvmria1v3wb9pcpkg2w";
-      }) {};
-      ignore-list = ''
-        /.stack-work
-      '';
-    in gitignore.gitignoreSourceAux ignore-list source;
+  ignoreStack = haskTools.ignoreSources ''
+    /.stack-work
+    '';
   #
   release = {
     inherit pkgs;
