ad803c5c300cb883831553633c5234537901f81d
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-05-22 14:51:15 +0300

    Fix SHA hashes

--- thundermint-crypto/sodium/Thundermint/Crypto/SHA.hs
+++ thundermint-crypto/sodium/Thundermint/Crypto/SHA.hs
@@ -1,12 +1,7 @@
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE DeriveDataTypeable         #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE TypeApplications           #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE TypeOperators              #-}
-{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE TypeFamilies       #-}
 -- |
 module Thundermint.Crypto.SHA (
     SHA1
@@ -15,22 +10,21 @@ module Thundermint.Crypto.SHA (
   , SHA512
   ) where
 
-import Data.Data             (Data)
+import Data.Data       (Data)
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Digest.Pure.SHA as SHA
 
 import Thundermint.Crypto
 
 
-----------------------------------------------------------------
---
-----------------------------------------------------------------
-
 -- | SHA1 hash function
 data SHA1  deriving (Data)
+
 
 instance ByteReprSized (Hash SHA1) where
   type ByteSize (Hash SHA1) = 20
 instance CryptoHash SHA1 where
-  hashBlob                   = Hash . undefined
+  hashBlob                   = Hash . BL.toStrict . SHA.bytestringDigest . SHA.sha1 . BL.fromStrict
   hashEquality (Hash hbs) bs = hbs == bs
 
 
@@ -40,7 +34,7 @@ data SHA256  deriving (Data)
 instance ByteReprSized (Hash SHA256) where
   type ByteSize (Hash SHA256) = 32
 instance CryptoHash SHA256 where
-  hashBlob                   = Hash . undefined
+  hashBlob                   = Hash . BL.toStrict . SHA.bytestringDigest . SHA.sha256 . BL.fromStrict
   hashEquality (Hash hbs) bs = hbs == bs
 
 
@@ -50,9 +44,8 @@ data SHA384  deriving (Data)
 instance ByteReprSized (Hash SHA384) where
   type ByteSize (Hash SHA384) = 48
 instance CryptoHash SHA384 where
-  hashBlob                   = Hash . undefined
+  hashBlob                   = Hash . BL.toStrict . SHA.bytestringDigest . SHA.sha384 . BL.fromStrict
   hashEquality (Hash hbs) bs = hbs == bs
-
 
 
 -- | SHA512 hash function
@@ -61,5 +54,5 @@ data SHA512  deriving (Data)
 instance ByteReprSized (Hash SHA512) where
   type ByteSize (Hash SHA512) = 64
 instance CryptoHash SHA512 where
-  hashBlob                   = Hash . undefined
+  hashBlob                   = Hash . BL.toStrict . SHA.bytestringDigest . SHA.sha512 . BL.fromStrict
   hashEquality (Hash hbs) bs = hbs == bs
