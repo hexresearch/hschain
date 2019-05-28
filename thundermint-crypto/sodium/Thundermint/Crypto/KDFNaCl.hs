2a67bda1e44feb0de9bf30d39a812aab79badbac
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-05-28 15:41:49 +0300

    Wrap key derivation functions

--- thundermint-crypto/sodium/Thundermint/Crypto/KDFNaCl.hs
+++ thundermint-crypto/sodium/Thundermint/Crypto/KDFNaCl.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CApiFFI            #-}
 {-# LANGUAGE DataKinds          #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE FlexibleInstances  #-}
@@ -5,14 +6,48 @@
 -- |
 module Thundermint.Crypto.KDFNaCl (KDFNaCl) where
 
+import Data.Data             (Data)
+import Data.Word
+import qualified Data.ByteArray  as Arr
+import qualified Data.ByteString as BS
+import Foreign.C.Types
+import Foreign.Ptr
+import System.IO.Unsafe
+
 import Thundermint.Crypto
 
 
+----------------------------------------------------------------
+--
+----------------------------------------------------------------
+
 data KDFNaCl
+  deriving Data
 
 instance ByteReprSized (KDFOutput KDFNaCl) where
   type ByteSize (KDFOutput KDFNaCl) = 32
 
 instance CryptoKDF KDFNaCl where
   type KDFParams KDFNaCl = ()
-  deriveKey () bs = undefined
+  deriveKey () bs
+    | BS.length bs < 32 = error "FIXME: don't know waht to do with short key"
+    | otherwise         = KDFOutput $ unsafePerformIO $ do
+        buf <-
+          Arr.alloc 32            $ \pOut  ->
+          Arr.withByteArray zeros $ \pZero ->
+          Arr.withByteArray bs    $ \pIn   ->
+          check =<< crypto_core_hsalsa20 pOut pZero pIn nullPtr
+        return $! buf
+
+zeros :: BS.ByteString
+zeros = BS.replicate 16 0
+----------------------------------------------------------------
+-- FFI
+----------------------------------------------------------------
+
+foreign import capi "sodium.h crypto_core_hsalsa20" crypto_core_hsalsa20
+  :: Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> IO CInt
+
+check :: CInt -> IO ()
+check 0 = return ()
+check _ = error "sodium error"
