05dee79674ebf3beea92624c932259a305db580e
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-06-10 12:26:29 +0300

    Avoid use of unsafeCoerce

--- thundermint-crypto/ghc/Thundermint/Crypto/KDFNaCl.hs
+++ thundermint-crypto/ghc/Thundermint/Crypto/KDFNaCl.hs
@@ -5,12 +5,12 @@
 -- |
 module Thundermint.Crypto.KDFNaCl where
 
-import Data.ByteArray        (convert,ScrubbedBytes)
-import Unsafe.Coerce
+import Data.ByteArray        (convert)
 
 import qualified Data.ByteArray           as Arr
 import qualified Data.ByteString          as BS
 import qualified Crypto.Cipher.XSalsa     as XSalsa
+import           Crypto.Cipher.Salsa        (State(..))
 
 import Thundermint.Crypto
 
@@ -29,10 +29,7 @@ instance CryptoKDF KDFNaCl where
     | BS.length bs < 32 = error "FIXME: don't know waht to do with short key"
     | otherwise         = KDFOutput kdf
     where
-      zeros  = BS.replicate 24 0
-      xsalsa = XSalsa.initialize 20 bs zeros
-      -- FIXME: Here we coerce newtype wrapper to its representation
-      array  = unsafeCoerce xsalsa :: ScrubbedBytes
-      chunk1 = Arr.view array 4  16
-      chunk2 = Arr.view array 44 16
-      kdf    = convert chunk1 <> convert chunk2
+      zeros       = BS.replicate 24 0
+      State array = XSalsa.initialize 20 bs zeros
+      kdf         = convert (Arr.view array 4  16)
+                 <> convert (Arr.view array 44 16)
