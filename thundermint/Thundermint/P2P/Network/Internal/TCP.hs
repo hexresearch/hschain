e0eaf22276da6906ce248174623ef87336579251
Yura Sheliah <yuriy.shelyag@gmail.com>
2019-06-14 17:58:51 +0200

    Moved local functions to module scope

--- thundermint/Thundermint/P2P/Network/Internal/TCP.hs
+++ thundermint/Thundermint/P2P/Network/Internal/TCP.hs
@@ -1,10 +1,11 @@
-module Thundermint.P2P.Network.Internal.TCP where
+module Thundermint.P2P.Network.Internal.TCP 
+  ( newNetworkTcp ) where
 
 import qualified Codec.Serialise as CBOR
 
 import Control.Monad          (when)
 import Control.Monad.Catch    (bracketOnError, throwM)
-import Control.Monad.IO.Class (liftIO)
+import Control.Monad.IO.Class (liftIO, MonadIO)
 import Data.List              (find)
 import Data.Monoid            ((<>))
 import Data.Word              (Word32)
@@ -32,15 +33,15 @@ newNetworkTcp peerInfo = (realNetworkStub peerInfo)
       addrs <- liftIO $ Net.getAddrInfo (Just hints) Nothing (Just serviceName)
       addr  <- case () of
         _ | Just a <- find isIPv6addr addrs -> return a
-          | a:_    <- addrs                    -> return a
-          | otherwise                          -> throwM NoAddressAvailable
+          | a:_    <- addrs                 -> return a
+          | otherwise                       -> throwM NoAddressAvailable
       --
       bracketOnError (liftIO $ newSocket addr) (liftIO . Net.close) $ \sock -> liftIO $ do
         when (isIPv6addr addr) $
           Net.setSocketOption sock Net.IPv6Only 0
         Net.bind sock (Net.addrAddress addr)
         Net.listen sock 5
-        return (liftIO $ Net.close sock, accept sock)
+        return (liftIO $ Net.close sock, accept peerInfo sock)
   --
   , connect  = \addr -> do
       let hints = Just Net.defaultHints
@@ -67,28 +68,37 @@ newNetworkTcp peerInfo = (realNetworkStub peerInfo)
   }
  where
   serviceName = show $ piPeerPort peerInfo
-  accept sock = do
-    (conn, addr) <- liftIO $ Net.accept sock
-    liftIO $ sendBS conn $ CBOR.serialise peerInfo
-    mbOtherPeerInfo <- liftIO $ recvBS conn
-    case fmap CBOR.deserialiseOrFail mbOtherPeerInfo of
-      Nothing -> fail $ "connection dropped while receiving peer info from " ++ show addr
-      Just (Left  _      ) -> fail $ "failure to decode PeerInfo from " ++ show addr
-      Just (Right otherPI) -> return (applyConn conn otherPI, sockAddrToNetAddr addr)
-  applyConn conn = P2PConnection (liftIO . sendBS conn) (liftIO $ recvBS conn) (liftIO $ Net.close conn)
-  sendBS sock =  \s -> NetLBS.sendAll sock (BB.toLazyByteString $ toFrame s)
-                 where
-                   toFrame msg = let len =  fromIntegral (LBS.length msg) :: Word32
-                                     hexLen = BB.word32BE len
-                                 in (hexLen <> BB.lazyByteString msg)
-  recvBS sock = do
-    header <- recvAll sock headerSize
-    if LBS.null header
-    then return Nothing
-    else let len = decodeWord16BE header
-         in case len of
-              Just n  -> Just <$> recvAll sock (fromIntegral n)
-              Nothing -> return Nothing
+
+accept :: (MonadIO m, CBOR.Serialise a)
+       => a -> Net.Socket -> m (P2PConnection, NetAddr)
+accept peerInfo sock = do
+  (conn, addr) <- liftIO $ Net.accept sock
+  liftIO $ sendBS conn $ CBOR.serialise peerInfo
+  mbOtherPeerInfo <- liftIO $ recvBS conn
+  case fmap CBOR.deserialiseOrFail mbOtherPeerInfo of
+    Nothing -> fail $ "connection dropped while receiving peer info from " ++ show addr
+    Just (Left  _      ) -> fail $ "failure to decode PeerInfo from " ++ show addr
+    Just (Right otherPI) -> return (applyConn conn otherPI, sockAddrToNetAddr addr)
+
+applyConn :: Net.Socket -> PeerInfo -> P2PConnection
+applyConn conn = P2PConnection (liftIO . sendBS conn) (liftIO $ recvBS conn) (liftIO $ Net.close conn)
+
+sendBS :: Net.Socket -> LBS.ByteString -> IO ()
+sendBS sock =  \s -> NetLBS.sendAll sock (BB.toLazyByteString $ toFrame s)
+               where
+                 toFrame msg = let len =  fromIntegral (LBS.length msg) :: Word32
+                                   hexLen = BB.word32BE len
+                               in (hexLen <> BB.lazyByteString msg)
+
+recvBS :: Net.Socket -> IO (Maybe LBS.ByteString)
+recvBS sock = do
+  header <- recvAll sock headerSize
+  if LBS.null header
+  then return Nothing
+  else let len = decodeWord16BE header
+       in case len of
+            Just n  -> Just <$> recvAll sock (fromIntegral n)
+            Nothing -> return Nothing
 
 
 -- | helper function read given length of bytes
