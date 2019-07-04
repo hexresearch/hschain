7e5db726ad2326331221d2f94ee25d76d552f810
romcheck <romcheck@gmail.com>
2019-07-04 11:26:25 +0200

    revert to scratch image

--- nix/containers.nix
+++ nix/containers.nix
@@ -14,15 +14,9 @@ let
   thundermint-exe     = lib.overrideCabal
     (lib.justStaticExecutables (lib.dontCheck pkgs.haskell.packages."${ghc}".thundermint)) (oldDerivation: { });
 
-  baseImage = pkgs.dockerTools.pullImage {
-      imageName = "alpine";
-      imageDigest = "sha256:e1871801d30885a610511c867de0d6baca7ed4e6a2573d506bbec7fd3b03873f";
-      sha256 = "05wcg38vsygjzf59cspfbb7cq98c7x18kz2yym6rbdgx960a0kyq";
-  };
-
   thundermint-docker = pkgs.dockerTools.buildImage {
     name = "${prefixName}thundermint-node";
-    fromImage = baseImage;
+    fromImageName = "scratch";
     tag = containerTag;
     created = "now";
     contents = [thundermint-exe];
