c8e65de9639eb9d62274329e7c7f5bdd6a46dc55
Dmitry Krylov <dima@krylov.ws>
2019-07-12 17:20:41 +1000

    Add some options and comments to thundermint-coin-node

--- thundermint/start.sh
+++ thundermint/start.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+#!/bin/bash -e
+##
+## Start some 'thundermint-coin-node' locally
+##
+
+# Additional arguments for running
+ARGS="+RTS -N4"
+
+# Clearing environment
+rm db/*
+rm logs/*
 
 trap 'kill 0' SIGINT
 
-thundermint-coin-node config/common.yaml config/local.yaml config/local1.yaml &
-thundermint-coin-node config/common.yaml config/local.yaml config/local2.yaml &
-thundermint-coin-node config/common.yaml config/local.yaml config/local3.yaml &
-thundermint-coin-node config/common.yaml config/local.yaml config/local4.yaml
+echo "Starting local thundermint-coin-node cluster"
+thundermint-coin-node config/common.yaml config/local.yaml config/local1.yaml $ARGS &
+thundermint-coin-node config/common.yaml config/local.yaml config/local2.yaml $ARGS &
+thundermint-coin-node config/common.yaml config/local.yaml config/local3.yaml $ARGS &
+thundermint-coin-node config/common.yaml config/local.yaml config/local4.yaml $ARGS
+
 kill 0
