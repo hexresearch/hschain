a91c01547e4cfbe63123b8f9c56ed750c28d8c65
romcheck <romcheck@gmail.com>
2019-07-12 17:34:27 +0200

    build-ci.sh push to nix cache trap

--- nix/build-ci.sh
+++ nix/build-ci.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-set -xe
+
+set -e
+shopt -s extglob
 
 # Configure cache
 ./setup-cache.sh
@@ -7,19 +9,16 @@ set -xe
 # Minimal nsswitch.conf
 [[ ! -e /etc/nsswitch.conf ]] && echo 'hosts: files dns' > /etc/nsswitch.conf
 
-# Debug version variables
-echo $DRONE_COMMIT
-echo $DRONE_BRANCH
-echo $DRONE_BUILD_NUMBER
+function push_nix_cache {
+    # Sign nix output
+    nix sign-paths -k /root/nix-cache-key.sec --all
+    # Upload to nix cache
+    echo /nix/store/!(*@(.drv|.lock)) | xargs nix-copy-closure -v --gzip --include-outputs --to root@cache.hxr.team
+}
+
+trap push_nix_cache EXIT
 
 # Build backend packages
 NIX_PATH=$GIT_NIX_PATH$NIX_PATH backend_output=$(nix-build --arg isProd true "$@")
 # Get runtime deps
 backend_deps=$(nix-store --query --requisites $backend_output)
-
-# Sign output
-nix sign-paths -k /root/nix-cache-key.sec --all
-
-# Upload to nix cache
-shopt -s extglob
-echo /nix/store/!(*@(.drv|.lock)) | xargs nix-copy-closure -v --gzip --include-outputs --to root@cache.hxr.team
