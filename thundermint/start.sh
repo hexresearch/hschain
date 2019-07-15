9e954dbec7ef9a1b61c0bb307c6d9140722ae70d
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-07-15 15:38:03 +0300

    Add parameters to start script

--- thundermint/start.sh
+++ thundermint/start.sh
@@ -2,8 +2,9 @@
 
 trap 'kill 0' SIGINT
 
-thundermint-coin-node config/common.yaml config/local.yaml config/local1.yaml &
-thundermint-coin-node config/common.yaml config/local.yaml config/local2.yaml &
-thundermint-coin-node config/common.yaml config/local.yaml config/local3.yaml &
-thundermint-coin-node config/common.yaml config/local.yaml config/local4.yaml
+export GHCRTS="-N8"
+thundermint-coin-node config/common.yaml config/local.yaml config/local1.yaml "$@" &
+thundermint-coin-node config/common.yaml config/local.yaml config/local2.yaml "$@" &
+thundermint-coin-node config/common.yaml config/local.yaml config/local3.yaml "$@" &
+thundermint-coin-node config/common.yaml config/local.yaml config/local4.yaml "$@"
 kill 0
