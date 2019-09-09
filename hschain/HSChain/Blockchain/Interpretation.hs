50de29324208c2f033b1de73d3e2b7b447c0ad18
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-09-09 16:56:32 +0300

    Documentation

--- hschain/HSChain/Blockchain/Interpretation.hs
+++ hschain/HSChain/Blockchain/Interpretation.hs
@@ -15,13 +15,23 @@ import HSChain.Types.Blockchain
 import HSChain.Blockchain.Internal.Engine.Types
 
 
+-- | Description of interpretation of blockchain state. Evaluation of
+--   blocks and transactions are done in the monad @q@ which is
+--   assumed to provide access to current state of blockchain
 data BChLogic q alg a = BChLogic
   { processTx     :: !(TX a -> q ())
+    -- ^ Process single transactions. Used when 
   , processBlock  :: !(Block alg a -> q ())
+    -- ^ Process and validate complete block.
   , generateBlock :: !(NewBlock alg a -> [TX a] -> q a)
+    -- ^ Generate block from list of transactions.
   , initialState  :: !(InterpreterState a)
+    -- ^ Initial state of blockchain
   }
 
+
+-- | Interpreter for mond in which evaluation of blockchain is
+--   performed
 newtype Interpreter q m alg a = Interpreter
   { interpretBCh :: forall x.
                     BlockchainState alg a
