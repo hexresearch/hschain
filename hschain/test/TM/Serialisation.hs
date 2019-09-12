89a48ce9a3bbc933760daa2047a0b4768718ddfc
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-09-12 16:42:36 +0300

    Clean up serialization tests
     - Add roundtrip tests for CBOR data
     - Rewrite tests for votes to use QC. They are much more conciese this way

--- hschain/test/TM/Serialisation.hs
+++ hschain/test/TM/Serialisation.hs
@@ -1,30 +1,22 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE LambdaCase          #-}
-{-# LANGUAGE RankNTypes          #-}
-{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeOperators       #-}
--- {-# LANGUAGE TemplateHaskell #-}
-
 -- | Tests for some serialisation things
 --
 module TM.Serialisation (tests) where
 
-import Codec.Serialise as C
-import qualified Data.Aeson as JSON
-import Data.Functor.Identity
+import qualified Codec.Serialise as CBOR
+import qualified Data.Aeson      as JSON
+import Data.Coerce
 import Data.Typeable
-import qualified Data.ByteString.Lazy as BSL
 
-import Hedgehog as H
-import Hedgehog.Internal.Exception as H
 import Test.QuickCheck.Arbitrary
 import Test.Tasty
-import Test.Tasty.Hedgehog
-import qualified Test.Tasty.QuickCheck as QC
-import qualified Hedgehog.Gen.QuickCheck as Gen
+import Test.Tasty.QuickCheck
 
 import HSChain.Crypto         ((:&))
 import HSChain.Crypto.Ed25519 (Ed25519)
@@ -34,112 +26,111 @@ import HSChain.P2P.Types      (NetAddr)
 
 import HSChain.Arbitrary.Instances ()
 
-type TestCryptoAlg     = Ed25519 :& SHA512
-type TestBlock         = String
-type TestVote ty       = Vote ty TestCryptoAlg TestBlock
-type TestVotePrevote   = Vote 'PreVote TestCryptoAlg TestBlock
-type TestVotePrecommit = Vote 'PreCommit TestCryptoAlg TestBlock
-
-
-testVotes :: Gen (TestVote ty)
-testVotes = Gen.arbitrary
-
-
-convertToOther :: TestVote a -> TestVote b
-convertToOther Vote{..} = Vote{..}
-
-
-prop_only_prevote_and_precommit :: Property
-prop_only_prevote_and_precommit = property $
-    forAll Gen.arbitrary >>= \case
-        PreVote   -> success
-        PreCommit -> success
-
-
-prop_can_serialize :: Property
-prop_can_serialize = property $ do
-    prop @TestVotePrevote
-    prop @TestVotePrecommit
-  where
-    prop :: forall ty . (Serialise ty, Show ty, Arbitrary ty) => PropertyT IO ()
-    prop = do
-        (vote :: ty) <- forAll (Gen.arbitrary)
-        H.assert $ not $ BSL.null $ C.serialise vote
-
-
-prop_can_deserialize :: Property
-prop_can_deserialize = property $ do
-    prop @TestVotePrevote
-    prop @TestVotePrecommit
-  where
-    prop :: forall ty . (Arbitrary ty, Eq ty, Serialise ty, Show ty) => PropertyT IO ()
-    prop = do
-        (vote :: ty) <- forAll (Gen.arbitrary)
-        H.tripping vote (C.serialise) (Identity . C.deserialise)
-
-
-prop_diff_serialize :: Property
-prop_diff_serialize = property $ do
-    (votePrevote :: TestVotePrevote) <- forAll testVotes
-    let votePrecommit = convertToOther @_ @'PreCommit votePrevote
-    C.serialise votePrevote /== C.serialise votePrecommit
-
-
-prop_diff_cant_serialize_to_other :: Property
-prop_diff_cant_serialize_to_other  = property $ do
-    (votePrevote :: TestVotePrevote) <- forAll testVotes
-    let (votePrecommit :: TestVotePrecommit) = C.deserialise (C.serialise votePrevote)
-    either (const success) (const failure) $ tryEvaluate votePrecommit
-
-prop_JSON_roundtrip :: (Eq a, JSON.FromJSON a, JSON.ToJSON a) => a -> Bool
-prop_JSON_roundtrip a = Just a == (JSON.decode . JSON.encode) a
-
-prop_ShowRead_roundtrip :: (Eq a, Show a, Read a) => a -> Bool
-prop_ShowRead_roundtrip a = a == (read . show) a
+----------------------------------------------------------------
+-- Test tree
+----------------------------------------------------------------
 
 tests :: TestTree
 tests =
-    testGroup "serialisation"
-        [ testGroup "Vote"
-            [ testProperty "only prevote and precommit"
-                           prop_only_prevote_and_precommit
-            , testProperty "can serialise"
-                           prop_can_serialize
-            , testProperty "can deserialise"
-                           prop_can_deserialize
-            , testProperty "serialize result for prevote and precommit differs"
-                           prop_diff_serialize
-            , testProperty "can't serialize to other"
-                           prop_diff_cant_serialize_to_other
-            ]
-        , let run :: forall a. ( Arbitrary a, Show a, Typeable a, Eq a
-                               , JSON.FromJSON a, JSON.ToJSON a)
-                  => TestTree
-              run = QC.testProperty (show (typeRep (Proxy @a)))
-                                    (prop_JSON_roundtrip @a)
-          in testGroup "JSON"
-             [ run @(Block  TestCryptoAlg Int)
-             , run @(Header TestCryptoAlg Int)
-             , run @(Commit TestCryptoAlg Int)
-             , run @(ByzantineEvidence TestCryptoAlg Int)
-             , run @(Proposal TestCryptoAlg Int)
-             , run @(Vote 'PreVote   TestCryptoAlg Int)
-             , run @(Vote 'PreCommit TestCryptoAlg Int)
-             , run @Height
-             , run @Round
-             , run @Time
-             , run @NetAddr
-             ]
-        , let run :: forall a. (Arbitrary a, Typeable a, Eq a, Show a, Read a)
-                  => TestTree
-              run = QC.testProperty (show (typeRep (Proxy @a)))
-                                    (prop_ShowRead_roundtrip @a)
-          in testGroup "Read/Show"
-             [ run @Height
-             , run @Round
-             , run @Time
-             , run @NetAddr
-             , QC.testProperty "NetAddr tcp://" $ \(a :: NetAddr) ->
-                 a == (read . ("tcp://"++) . show) a
-             ]
-        ]
+  testGroup "serialisation"
+    [ let run :: forall a. ( Arbitrary a, Show a, Typeable a, Eq a, CBOR.Serialise a)
+              => TestTree
+          run = testProperty (show (typeRep (Proxy @a)))
+                                (prop_CBOR_roundtrip @a)
+      in testGroup "CBOR"
+         [ testGroup "roundtrip"
+           [ run @(Block  TestCryptoAlg Int)
+           , run @(Header TestCryptoAlg Int)
+           , run @(Commit TestCryptoAlg Int)
+           , run @(ByzantineEvidence TestCryptoAlg Int)
+           , run @(Proposal TestCryptoAlg Int)
+           , run @(Vote 'PreVote   TestCryptoAlg Int)
+           , run @(Vote 'PreCommit TestCryptoAlg Int)
+           , run @Height
+           , run @Round
+           , run @Time
+           , run @NetAddr
+           ]
+         , testGroup "Vote"
+           [ testProperty "serialize result for prevote and precommit differs"
+             prop_diff_serialize
+           , testProperty "cannot deserialise one vote type as other PV->PC"
+             (prop_diff_votes_dont_roundtrip @'PreVote)
+           , testProperty "cannot deserialise one vote type as other PC->PV"
+             (prop_diff_votes_dont_roundtrip @'PreCommit)
+           ]
+         ]
+    , let run :: forall a. ( Arbitrary a, Show a, Typeable a, Eq a
+                           , JSON.FromJSON a, JSON.ToJSON a)
+              => TestTree
+          run = testProperty (show (typeRep (Proxy @a)))
+                                (prop_JSON_roundtrip @a)
+      in testGroup "JSON"
+         [ run @(Block  TestCryptoAlg Int)
+         , run @(Header TestCryptoAlg Int)
+         , run @(Commit TestCryptoAlg Int)
+         , run @(ByzantineEvidence TestCryptoAlg Int)
+         , run @(Proposal TestCryptoAlg Int)
+         , run @(Vote 'PreVote   TestCryptoAlg Int)
+         , run @(Vote 'PreCommit TestCryptoAlg Int)
+         , run @Height
+         , run @Round
+         , run @Time
+         , run @NetAddr
+         ]
+    , let run :: forall a. (Arbitrary a, Typeable a, Eq a, Show a, Read a)
+              => TestTree
+          run = testProperty (show (typeRep (Proxy @a)))
+                                (prop_ShowRead_roundtrip @a)
+      in testGroup "Read/Show"
+         [ run @Height
+         , run @Round
+         , run @Time
+         , run @NetAddr
+         , testProperty "NetAddr tcp://" $ \(a :: NetAddr) ->
+             a == (read . ("tcp://"++) . show) a
+         ]
+    ]
+
+----------------------------------------------------------------
+-- Tests
+----------------------------------------------------------------
+
+type TestCryptoAlg = Ed25519 :& SHA512
+type TestVote ty   = Vote ty TestCryptoAlg String
+
+
+
+-- Votes of one type can't be deserialized as other
+prop_diff_votes_dont_roundtrip
+  :: forall ty. ( CBOR.Serialise (TestVote ty)
+                , CBOR.Serialise (TestVote (OtherVote ty)))
+  => TestVote ty -> Bool
+prop_diff_votes_dont_roundtrip vote =
+  case CBOR.deserialiseOrFail (CBOR.serialise vote) of
+    Left   _                             -> True
+    Right (_ :: TestVote (OtherVote ty)) -> False
+
+type family OtherVote x where
+  OtherVote 'PreVote   = 'PreCommit
+  OtherVote 'PreCommit = 'PreVote
+
+
+-- Prevotes and precommits are serialized differently
+prop_diff_serialize :: TestVote 'PreVote -> Bool
+prop_diff_serialize prevote
+  = CBOR.serialise prevote /= CBOR.serialise precommit
+  where
+    precommit = coerce prevote :: TestVote 'PreCommit
+
+-- CBOR.deserialise . CBOR.serialise
+prop_CBOR_roundtrip :: (Eq a, CBOR.Serialise a) => a -> Bool
+prop_CBOR_roundtrip a = a == (CBOR.deserialise . CBOR.serialise) a
+
+-- JSON.decode . JSON.encode = id
+prop_JSON_roundtrip :: (Eq a, JSON.FromJSON a, JSON.ToJSON a) => a -> Bool
+prop_JSON_roundtrip a = Just a == (JSON.decode . JSON.encode) a
+
+-- Read . show = id
+prop_ShowRead_roundtrip :: (Eq a, Show a, Read a) => a -> Bool
+prop_ShowRead_roundtrip a = a == (read . show) a
