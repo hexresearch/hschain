4065ef57c4aaaf4360cda237d78d18303abc61f2
Serguey Zefirov <sergueyz@gmail.com>
2019-11-05 15:02:31 +0300

    Accepting connections, threads to read requests, mock code

--- sql-interface/python/mock-consensus-node.tcl
+++ sql-interface/python/mock-consensus-node.tcl
@@ -5,7 +5,39 @@
 # and has better TCP/IP support.
 # 
 
+package require Thread
 package require sqlite3
+
+set current_requests [list ]
+set requests_performed [list ]
+
+proc height {} {
+	global requests_performed
+	return [llength $requests_performed]
+}
+
+proc new_height {} {
+	global current_requests requests_performed
+	lappend requests_performed $current_requests
+	set current_requests [list ]
+}
+
+proc ascend_action {} {
+	new_height
+	after 1000 ascend_action
+}
+
+proc accept_connection {socket address port} {
+	set thread_script {
+	package require thread
+	# the procedure that reads requests from specified socket
+	# and calls (thread::send) main thread to take action.
+	proc read_requests {socket main_id} {
+		
+	}
+}
+	thread::create "$thread_script\n\nread_requests $socket [thread::id]"
+}
 
 proc main {argv0 argv} {
 	if {[llength $argv] != 2} {
@@ -21,6 +53,11 @@ proc main {argv0 argv} {
 	puts "database created"
 	database eval $mandatory_tables
 	puts "tables added."
+	after 1000 ascend_action
+	puts "accepting connections."
+	socket -server accept_connection $port
 }
 
 main $argv0 $argv
+
+thread::wait
