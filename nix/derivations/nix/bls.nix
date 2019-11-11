f79ca49b2adb7953f1961e5af99e34b498d64a96
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-11-11 13:59:17 +0300

    Build BLS with libgmp

--- nix/derivations/nix/bls.nix
+++ nix/derivations/nix/bls.nix
@@ -1,7 +1,8 @@
-{ stdenv, fetchgit, cmake, python }:
+{ stdenv, fetchgit, cmake, python, gmp }:
 stdenv.mkDerivation rec {
   name    = "bls";
   version = "0.1";
+  buildInputs       = [ gmp ];
   nativeBuildInputs = [ cmake python ];
 
   # We need to clone submodules as well.
@@ -13,6 +14,9 @@ stdenv.mkDerivation rec {
     rev       = "93e4f4118b326e7f5e2bec1445cc68aac0728026";
     sha256    = "1ns76irr34myr0vzmbw5yw22m7vpg91b1la8s5qg6asngvrdvzik";
   };
+  # Patch out linking for GMP. We still build with GMP but don't link
+  # it since it causes problems when linking haskell programs
+  patches = [ ./remove-gmp-from-linking.patch ];
   # chiabls/bls.hpp uses:
   #
   # > #include "relic_conf.h"
