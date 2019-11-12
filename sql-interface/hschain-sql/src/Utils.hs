12f6763a0d31a59788dc68fcb974e9cf2c655370
Serguey Zefirov <sergueyz@gmail.com>
2019-11-12 10:11:53 +0300

    Progress keeping - about to get good genesis

--- sql-interface/hschain-sql/src/Utils.hs
+++ sql-interface/hschain-sql/src/Utils.hs
@@ -16,8 +16,7 @@ import Options.Applicative
 sqlStr :: String -> String
 sqlStr = show . concatMap escape
   where
-    escape '"' = "\\\""
-    escape '\'' = "\\'"
+    escape '\'' = "''"
     escape c = [c]
 
 walletDemoTableName :: String
@@ -29,10 +28,11 @@ sqlDialect :: Dialect
 sqlDialect = ansi2011
 
 addStatements :: [String] -> SQLGenMonad ()
+addStatements [] = return ()
 addStatements ss = do
   let txt = unlines ss
-  case parseStatements sqlDialect "" Nothing txt of
-    Left err -> error $ "internal error: statements parsing failure: " ++ show err
+  case parseStatements sqlDialect "" Nothing $ unlines ss of
+    Left err -> error $ "internal error: statements parsing failure: " ++ show err++ "\nstatements:\n--------------\n" ++ unlines ss ++"--------------"
     Right _ -> modify $ \ssPrev  -> ssPrev ++ ss
 
 normalizeStatementString :: String -> String
@@ -46,7 +46,7 @@ normalizeStatementString cs = reduceSpaces $ onlySpaces cs
 commandAction :: Command -> SQLGenMonad ()
 commandAction (MandatorySystemTables userTables initialRequests) = do
   addStatements userTables
-  addStatements
+  addStatements $
     [ "-- special table - current height. keep it single-valued."
     , "CREATE TABLE height (height INTEGER);"
     , ""
@@ -63,10 +63,48 @@ commandAction (MandatorySystemTables userTables initialRequests) = do
     , "    , request_param_type STRING"
     , "    , CONSTRAINT unique_param_for_request UNIQUE (request_id, request_param_name)"
     , "    , CONSTRAINT request_must_exist FOREIGN KEY (request_id) REFERENCES allowed_requests(request_id)"
-    , "    , CONSTRAINT correct_type CHECK (request_param_type = \"S\" OR request_param_type = \"I\" OR request_param_type = \"P\")"
+    , "    , CONSTRAINT correct_type CHECK (request_param_type = 'S' OR request_param_type = 'I' OR request_param_type = 'P')"
     , "    );"
     ]
-    addStatements userRequests
+  addStatements initialRequests
+  addStatements
+    [ "-- special table that keeps genesis requests"
+    , "CREATE TABLE serialized_genesis_requests"
+    , "    ( seq_index   INTEGER"
+    , "    , request_sql STRING"
+    , "    );"
+    , ""
+    , "-- special table that keeps serialized requests"
+    , "CREATE TABLE serialized_requests"
+    , "    ( height     INTEGER -- height for request"
+    , "    , seq_index  INTEGER -- order inside the height"
+    , "    , request    STRING  -- the request. must be request_id for height >= 0 and SQK text for genesis."
+    , "    , CONSTRAINT serialized_requests_primary_key PRIMARY KEY (height, seq_index)"
+    , "    , CONSTRAINT must_have_request_id FOREIGN KEY (request_id) REFERENCES allowed_requests(request_id)"
+    , "    );"
+    , ""
+    , "-- special table that keeps actual parameter values for seriqlized requests"
+    , "CREATE TABLE serialized_requests_values"
+    , "    ( height     INTEGER -- height for request"
+    , "    , seq_index  INTEGER -- order inside the height"
+    , "    , request_id STRING  -- the text itself. must be request_id"
+    , "    , request_param_name  STRING -- name of the parameter"
+    , "    , request_param_value STRING -- and value of the parameter, interpreted according to the type from allowed_requests_params table"
+    , "    , CONSTRAINT serialized_requests_values_primary_key PRIMARY KEY (height, seq_index, request_param_name)"
+    , "    , CONSTRAINT must_have_request FOREIGN KEY (height, seq_index, request_id) REFERENCES serialized_requests(height, seq_index, request_id)"
+    , "    );"
+    ]
+  ss <- get
+  case parseStatements sqlDialect "" Nothing $ unlines ss of
+    Left err -> error $ "internal error: error parsing combined statements: "++show err
+    Right statements -> do
+      let printedBack = map normalizeStatementString $
+                            map (prettyStatement sqlDialect) statements
+      addStatements
+        [ "INSERT INTO serialized_genesis_requests " ++
+          "(request_sql, seq_index) VALUES ("++sqlStr req++", "++show seqIndex++");"
+        | (req, seqIndex) <- zip printedBack [0..]
+        ]
 commandAction (AddRequestCode req id params) = do
   let sqlId = sqlStr id
       sqlReq = sqlStr $ normalizeStatementString req
@@ -86,80 +124,9 @@ commandAction (AddRequestCode req id params) = do
             [ "INSERT INTO allowed_requests_params"
             , "    (request_id, request_param_type, request_param_name)"
             , "  VALUES"
-            , "    ("++sqlId++", "++show ptypeStr++", "++show pname++");"
+            , "    ("++sqlId++", "++sqlStr ptypeStr++", "++sqlStr pname++");"
             ]
     ]
-{-
-commandAction (WalletDemoTables serverSide) = do
-  commandAction $ MandatorySystemTables $
-    [ "-- funds available"
-    , "CREATE TABLE "++walletDemoTableName
-    , "  ( wallet_id STRING PRIMARY KEY"
-    , "  , amount    INTEGER"
-    , "  , CONSTRAINT no_overdraft CHECK (amount >= 0)"
-    , "  );"
-    ] ++
-    [ unwords
-        [ "INSERT INTO "++walletDemoTableName
-        , "(wallet_id, amount)"
-        , "VALUES ("++sqlStr id++", "++show amount++");"
-        ]
-    | (id, amount) <- map (flip (,) 1000000) ["u1", "u2", "u3", "u4"]
-    ]
-
-  let req = unwords
-              [ "UPDATE "++walletDemoTableName
-              , "SET amount = CASE"
-              ,              "WHEN wallet_id = :user_id      THEN amount - :transfer_amount"
-              ,              "WHEN address = :dest_user_id THEN amount - :transfer_amount"
-              ,              "END"
-              , "WHERE     (address = :user_id OR address = :dest_user_id)"
-              , "          AND :transfer_amount > 0;"
-              ]
-  commandAction $ AddRequestCode req "transfer"
-      [ (PositiveParam, "transfer_amount")
-      , (StringParam,   "user_id")
-      ]
-  when serverSide $ do
-    addStatements
-      [ "-- special table that keeps genesis requests"
-      , "CREATE TABLE serialized_genesis_requests"
-      , "    ( order       INTEGER"
-      , "    , request_sql STRING"
-      , "    );"
-      , ""
-      , "-- special table that keeps serialized requests"
-      , "CREATE TABLE serialized_requests"
-      , "    ( height     INTEGER -- height for request"
-      , "    , order      INTEGER -- order inside the height"
-      , "    , request    STRING  -- the request. must be request_id for height >= 0 and SQK text for genesis."
-      , "    , CONSTRAINT serialized_requests_primary_key PRIMARY KEY (height, order)"
-      , "    , CONSTRAINT must_have_request_id FOREIGN KEY (request_id) REFERENCES allowed_requests(request_id)"
-      , "    );"
-      , ""
-      , "-- special table that keeps actual parameter values for seriqlized requests"
-      , "CREATE TABLE serialized_requests_values"
-      , "    ( height     INTEGER -- height for request"
-      , "    , order      INTEGER -- order inside the height"
-      , "    , request_id STRING  -- the text itself. must be request_id"
-      , "    , request_param_name  STRING -- name of the parameter"
-      , "    , request_param_value STRING -- and value of the parameter, interpreted according to the type from allowed_requests_params table"
-      , "    , CONSTRAINT serialized_requests_values_primary_key PRIMARY KEY (height, order, request_param_name)"
-      , "    , CONSTRAINT must_have_request FOREIGN KEY (height, order, request_id) REFERENCES serialized_requests(height, order, request_id)"
-      , "    );"
-      ]
-    ss <- get
-    case parseStatements sqlDialect "" Nothing $ unlines ss of
-      Left err -> error $ "internal error: error parsing combined statements: "++show err
-      Right statements -> do
-        let printedBack = map normalizeStatementString $
-                              map (prettyStatement sqlDialect) statements
-        addStatements
-          [ "INSERT INTO serialized_genesis_requests " ++
-              "(order, request_sql) VALUES ("++sqlStr req++", "++show order++");"
-          | (req, order) <- zip printedBack [0..]
-          ]
--}
 
 main :: IO ()
 main = do
@@ -178,12 +145,12 @@ parseCommand = subparser
   where
     requestText = strOption (long "request")
     requestId = strOption (long "id")
-    serverFlag = switch (long "server-side")
     typedParam :: Parser (PType, String)
     typedParam =
           ((,) IntParam <$> strOption (long "int"))
       <|> ((,) StringParam <$> strOption (long "string"))
       <|> ((,) PositiveParam <$> strOption (long "positive"))
     sqlOption opt = option (eitherReader tryToParseSQL) (long opt)
-    tryToParseSQL s = either (Left . show) (Right . const s) $
-                        parseStatement sqlDialect "" Nothing s
+    tryToParseSQL s = either (Left . (++s) . (++ "\n--------------\n") . show)
+                        (Right . const s) $
+                        parseStatements sqlDialect "" Nothing s
--- sql-interface/python/run-mock-consensus.sh
+++ sql-interface/python/run-mock-consensus.sh
@@ -5,24 +5,24 @@ IFS=$'\n\t'
 
 walletDemoTableName=funds
 
-create_populate_funds="-- funds available\
-CREATE TABLE $walletDemoTableName \
-  ( wallet_id STRING PRIMARY KEY \
-  , amount    INTEGER \
-  , CONSTRAINT no_overdraft CHECK (amount >= 0) \
-  ); \
-INSERT INTO $walletDemoTableName \
-        , "(wallet_id, amount) \
-        , "VALUES (\"u1\", 1000000); \
-INSERT INTO $walletDemoTableName \
-        , "(wallet_id, amount) \
-        , "VALUES (\"u2\", 1000000); \
-INSERT INTO $walletDemoTableName \
-        , "(wallet_id, amount) \
-        , "VALUES (\"u3\", 1000000); \
-INSERT INTO $walletDemoTableName \
-        , "(wallet_id, amount) \
-        , "VALUES (\"u4\", 1000000); \
+create_populate_funds="-- funds available
+CREATE TABLE $walletDemoTableName
+  ( wallet_id STRING PRIMARY KEY
+  , amount    INTEGER
+  , CONSTRAINT no_overdraft CHECK (amount >= 0)
+  );
+INSERT INTO $walletDemoTableName
+            (wallet_id, amount)
+            VALUES (\"u1\", 1000000);
+INSERT INTO $walletDemoTableName
+            (wallet_id, amount)
+            VALUES (\"u2\", 1000000);
+INSERT INTO $walletDemoTableName
+            (wallet_id, amount)
+            VALUES (\"u3\", 1000000);
+INSERT INTO $walletDemoTableName
+            (wallet_id, amount)
+            VALUES (\"u4\", 1000000);
 "
 
 request="\
@@ -33,10 +33,11 @@ WHEN address = :dest_user_id THEN amount - :transfer_amount \
 END \
 WHERE     (address = :user_id OR address = :dest_user_id) \
 AND :transfer_amount > 0;"
-params= $(cabal new-exec -- hschain-sql-utils add-request-code  req "transfer"
-      [ (PositiveParam, "transfer_amount")
-      , (StringParam,   "user_id")
-      ]
+
+add_transfer_request=$(cabal new-exec -- hschain-sql-utils add-request-code \
+  --request "$request" --id "transfer" --positive transfer_amount \
+  --string user_id
+  )
 
 tclsh mock-consensus-node.tcl 22222                            \
-        "cabal new-exec -- hschain-sql-utils mandatory-system-tables --sql "$request" $params"
+        "cabal new-exec -- hschain-sql-utils mandatory-system-tables --table \"$create_populate_funds\" --request \"$add_transfer_request\""
