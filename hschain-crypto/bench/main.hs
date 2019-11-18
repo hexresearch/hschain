10c4803107c297ac2cc5d6efab52b9d2c555c7c8
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-11-18 16:22:38 +0300

    Add benchmarks for hashes

--- hschain-crypto/bench/main.hs
+++ hschain-crypto/bench/main.hs
@@ -4,6 +4,7 @@ module Main where
 import Criterion.Main
 
 import qualified Ben.Ed25519
+import qualified Ben.Hash
 #if USE_BLS
 import qualified Ben.BLS
 #endif
@@ -11,6 +12,7 @@ import qualified Ben.BLS
 main :: IO ()
 main = defaultMain
   [ Ben.Ed25519.benchmarks
+  , Ben.Hash.benchmarks
 #if USE_BLS
   , Ben.BLS.benchmarks
 #endif
