3abef01927b63fed3a258fd4525b5e9375ce5d47
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-11-18 12:10:13 +0300

    Split Ed25519 tests

--- hschain-crypto/bench/main.hs
+++ hschain-crypto/bench/main.hs
@@ -3,22 +3,14 @@ module Main where
 
 import Criterion.Main
 
-
---import Control.DeepSeq
---import Control.Exception (evaluate)
-
---import Crypto.Random              (getRandomBytes)
---import HSChain.Crypto
---import HSChain.Crypto.Ed25519
-
-
+import qualified Ben.Ed25519
 #if USE_BLS
 import qualified Crypto
 #endif
 
 main :: IO ()
 main = defaultMain
-  [
+  [ Ben.Ed25519.benchmarks
 #if USE_BLS
     Crypto.benchmarks
 #endif
