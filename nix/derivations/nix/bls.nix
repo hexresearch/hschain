37db5b587caca27e287c757e699d666d89917ce6
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-11-27 12:13:29 +0300

    Fix unreproducibility in bls-signatures
    Submodule for python bindings wasn't pinned so when we fetched sourse we get
    unreproducible results: latest pybind11 master
    
    Solution is simple - pactch out python bindings from build process

--- nix/derivations/nix/bls.nix
+++ nix/derivations/nix/bls.nix
@@ -5,18 +5,17 @@ stdenv.mkDerivation rec {
   buildInputs       = [ gmp ];
   nativeBuildInputs = [ cmake python ];
 
-  # We need to clone submodules as well.
-  #
-  # deepClone is workaround for that
   src = fetchgit {
     url       = "https://github.com/Chia-Network/bls-signatures";
-    deepClone = true;
     rev       = "93e4f4118b326e7f5e2bec1445cc68aac0728026";
-    sha256    = "1ns76irr34myr0vzmbw5yw22m7vpg91b1la8s5qg6asngvrdvzik";
+    sha256    = "13x9rvsfdpjhwrgbzr08c27633dz5gvlgh4974x2r0chrz6q94xd";
   };
   # Patch out linking for GMP. We still build with GMP but don't link
   # it since it causes problems when linking haskell programs
-  patches = [ ./remove-gmp-from-linking.patch ];
+  patches = [
+    ./0001-Remove-GMP-linking.patch
+    ./0002-Patch-out-building-of-ython-bindings.patch
+    ];
   # chiabls/bls.hpp uses:
   #
   # > #include "relic_conf.h"
