09b0b6d1d0ddc41f8c719f591ea14ff819f9462a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-12-18 20:08:08 +0300

    Add ahrness for running benchmark
    We'll decide what to do with it later

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+trap 'kill 0' SIGINT
+export GHCRTS="-N8"
+#
+taskset -c 0-3   hschain-dioxane-node config/common.yaml config/local1.yaml "$@" > /dev/null &
+taskset -c 4-7   hschain-dioxane-node config/common.yaml config/local2.yaml "$@" > /dev/null &
+taskset -c 8-11  hschain-dioxane-node config/common.yaml config/local3.yaml "$@" > /dev/null &
+taskset -c 12-15 hschain-dioxane-node config/common.yaml config/local4.yaml "$@" > /dev/null
+kill 0
+
