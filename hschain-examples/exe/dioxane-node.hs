39955bab300e0c3b46f8a81e2c5ef11befb4cbab
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-12-23 22:09:00 +0300

    Add prometheus metrics for dioxane

--- hschain-examples/exe/dioxane-node.hs
+++ hschain-examples/exe/dioxane-node.hs
@@ -11,29 +11,42 @@
 {-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE TypeOperators              #-}
-import Control.Arrow ((&&&))
+import Control.Arrow      ((&&&))
+import Control.Concurrent (forkIO)
+import Control.Monad
+import Control.Monad.Catch
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Cont
+import Control.Monad.Trans.Reader
 import Data.Aeson             (FromJSON)
 import Data.Monoid            ((<>))
 import Data.Word
 import qualified Data.Vector as V
 import Data.Yaml.Config       (loadYamlSettings, requireEnv)
 import Options.Applicative
+
+import Network.Wai.Middleware.Prometheus
+import Prometheus   (register)
+import Prometheus.Metric.GHC
+import qualified Network.Wai.Handler.Warp as Warp
+
 import GHC.Generics (Generic)
 
 import HSChain.Blockchain.Internal.Engine.Types
-import HSChain.Logger
-import HSChain.Mock.Dioxane
-import HSChain.Mock.Types
-import HSChain.Run
 import HSChain.Control
-import HSChain.Store
-import HSChain.Mock
-import HSChain.Mock.KeyList
 import HSChain.Crypto         (publicKey)
+import HSChain.Debug.Trace
+import HSChain.Logger
+import HSChain.Mock
+import HSChain.Mock.Dioxane
+import HSChain.Mock.KeyList
+import HSChain.Mock.Types
+import HSChain.Monitoring
 import HSChain.P2P            (generatePeerId)
 import HSChain.P2P.Network    (newNetworkTcp)
+import HSChain.Run
+import HSChain.Store
 import HSChain.Types
 
 import qualified HSChain.P2P.Types as P2PT
@@ -43,12 +56,28 @@ import qualified HSChain.P2P.Types as P2PT
 --
 ----------------------------------------------------------------
 
+newtype MonitorT m a = MonitorT (ReaderT PrometheusGauges m a)
+  deriving ( Functor,Applicative,Monad
+           , MonadIO,MonadMask,MonadThrow,MonadCatch
+           , MonadLogger,MonadFork,MonadTrace )
+
+instance MonadIO m =>  MonadTMMonitoring (MonitorT m) where
+  usingCounter getter n   = MonitorT $ flip addCounterNow n =<< asks getter
+  usingGauge   getter x   = MonitorT $ flip setTGaugeNow x =<< asks getter
+  usingVector  getter l x = MonitorT $ do
+    g <- asks getter
+    setTGVectorNow g l x
+
+runMonitorT :: PrometheusGauges -> MonitorT m a -> m a
+runMonitorT g (MonitorT m) = runReaderT m g
+
+
 data DioTag
 
 instance Dio DioTag where
   dioDict = DioDict
     { dioUserKeys       = V.fromList
-                        $ take 5000
+                        $ take 10
                         $ map (id &&& publicKey)
                         $ makePrivKeyStream 1337
     , dioInitialBalance = 1000000
@@ -84,6 +113,7 @@ main = do
   -- NOTE: later files take precedence
   nspec@NodeSpec{} :*: NodeCfg{..} :*: (cfg :: Configuration Example)
     <- loadYamlSettings (reverse cmdConfigPath) [] requireEnv
+  startWebMonitoring $ fromIntegral nodePort + 1000
   -- Start node
   evalContT $ do
     -- Create network
@@ -94,8 +124,8 @@ main = do
                                  }
     --
     (conn, logenv) <- allocNode nspec
-    let run = runLoggerT logenv . runDBT conn
-    lift $ run $ do
+    gauges         <- standardMonitoring
+    lift $ runMonitorT gauges . runLoggerT logenv . runDBT conn $ do
       (RunningNode{..},acts) <- interpretSpec @_ @_ @DioTag
         (nspec :*: cfg :*: bnet)
         nodeIdx
@@ -116,3 +146,11 @@ parser = do
     <> showDefault
     )
   return Opts{..}
+
+startWebMonitoring :: Warp.Port -> IO ()
+startWebMonitoring port = do
+    void $ register ghcMetrics
+    void $ forkIO
+         $ Warp.run port
+         $ prometheus def { prometheusInstrumentPrometheus = False }
+                      metricsApp
