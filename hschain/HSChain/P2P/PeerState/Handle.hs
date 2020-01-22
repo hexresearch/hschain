339e91a7417389f5a4e7cff53cf62ced9096d4fa
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-01-22 16:37:58 +0300

    Partially abstract disptach over different HandlerDicts

--- hschain/HSChain/P2P/PeerState/Handle.hs
+++ hschain/HSChain/P2P/PeerState/Handle.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs            #-}
-
+{-# LANGUAGE RankNTypes       #-}
 module HSChain.P2P.PeerState.Handle
  ( module HSChain.P2P.PeerState.Types
  , handler
@@ -29,13 +29,7 @@ handler :: (CryptoHashable a, HandlerCtx a m)
         -> State a
         -> GossipTimeout
         -> m (State a, [Command a])
-handler config st event = do
-  (st',cmds) <- case st of
-    Lagging s -> runTransitionT (handlerGeneric Lagging.handler event) config s
-    Current s -> runTransitionT (handlerGeneric Current.handler event) config s
-    Ahead   s -> runTransitionT (handlerGeneric Ahead.handler   event) config s
-    Unknown s -> runTransitionT (handlerGeneric Unknown.handler event) config s
-  second (cmds <>) <$> handleIssuedGossip config st' [ c | Push2Gossip c <- cmds ]
+handler = dispatchDictionaries handlerGeneric
 
 handlerTx
   :: (CryptoHashable a, HandlerCtx a m)
@@ -52,23 +46,14 @@ handlerTx config st msgTx = do
       TxPreVote   v -> GossipPreVote   v
       TxPreCommit v -> GossipPreCommit v
 
-
 handlerGossip
   :: (CryptoHashable a, HandlerCtx a m)
   => Config a
   -> State a
   -> GossipMsg a
   -> m (State a, [Command a])
-handlerGossip config st event = do
-  (st',cmds) <- case st of
-    Lagging s -> runTransitionT (handlerG Lagging.handler event) config s
-    Current s -> runTransitionT (handlerG Current.handler event) config s
-    Ahead   s -> runTransitionT (handlerG Ahead.handler   event) config s
-    Unknown s -> runTransitionT (handlerG Unknown.handler event) config s
-  second (cmds <>) <$> handleIssuedGossip config st' [ c | Push2Gossip c <- cmds ]
-  where
-    handlerG dct m = do resendGossip m
-                        handlerGossipMsg dct m
+handlerGossip = dispatchDictionaries $ \dct m ->
+  resendGossip m >> handlerGossipMsg dct m
 
 handleIssuedGossip :: (CryptoHashable a, HandlerCtx a m)
                    => Config a
@@ -80,3 +65,20 @@ handleIssuedGossip config st = foldM (\ (s',cmds) msg -> second (cmds<>) <$> cas
   Current s -> runTransitionT (issuedGossipHandlerGeneric Current.handler msg) config s
   Ahead   s -> runTransitionT (issuedGossipHandlerGeneric Ahead.handler   msg) config s
   Unknown s -> runTransitionT (issuedGossipHandlerGeneric Unknown.handler msg) config s ) (st,[])
+
+
+dispatchDictionaries
+  :: ( CryptoHashable a, HandlerCtx a m
+     )
+  => (forall s. HandlerDict s a m -> msg -> TransitionT s a m ())
+  -> Config a
+  -> State a
+  -> msg
+  -> m (State a, [Command a])
+dispatchDictionaries fun config st event = do
+  (st',cmds) <- case st of
+    Lagging s -> runTransitionT (fun Lagging.handler event) config s
+    Current s -> runTransitionT (fun Current.handler event) config s
+    Ahead   s -> runTransitionT (fun Ahead.handler   event) config s
+    Unknown s -> runTransitionT (fun Unknown.handler event) config s
+  second (cmds <>) <$> handleIssuedGossip config st' [ c | Push2Gossip c <- cmds ]
