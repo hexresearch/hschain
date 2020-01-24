1dd52204c3ec48306225df61c3217c17a851a77c
Serguey Zefirov <sergueyz@gmail.com>
2020-01-24 16:36:33 +0300

    Test and implementation skeletons

---
+++ proof-of-work/SAT-experiments/proto/evpow.c
@@ -0,0 +1,17 @@
+/* EVPOW implementation.
+ *
+ */
+
+#include "evpow.h"
+
+int
+evpow_solve( uint8_t* prefix
+	   , size_t prefix_size
+	   , uint8_t* answer
+	   , uint8_t* solution_hash
+	   , int complexity_shift
+	   , uint16_t complexity_mantissa
+) {
+	return 0;
+} /* evpow_solve */
+
---
+++ proof-of-work/SAT-experiments/proto/test_evpow.c
@@ -0,0 +1,13 @@
+/* test for EVPOW algorithm.
+ *
+ * We test here for:
+ *  - probability of success for different complexities
+ *  - time to find a refutation and/or solution
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	return 0;
+} /* main */
+
