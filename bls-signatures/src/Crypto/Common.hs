ec396f0772ba7091e68e9132c3d12497a72b2ada
Dmitry Krylov <dima@krylov.ws>
2020-01-29 19:24:45 +1000

    Update `bls-signatures` raw API for WASM - continue

---
+++ bls-signatures/src/Crypto/Common.hs
@@ -0,0 +1,17 @@
+-- TODO mkdir Internal and move other files to this
+module Crypto.Common where
+
+publicKeySize :: Int
+publicKeySize = 48
+
+
+privateKeySize :: Int
+privateKeySize = 32
+
+signatureSize :: Int
+signatureSize = 96
+
+hashSize :: Int
+hashSize = 96
+
+
