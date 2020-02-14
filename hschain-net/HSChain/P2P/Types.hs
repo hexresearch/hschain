ffca60c66b50f401a4564345d9ce9b36a3591b35
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-02-14 20:34:34 +0300

    Move definitions to HSChain.Network.Types

--- hschain-net/HSChain/P2P/Types.hs
+++ hschain-net/HSChain/P2P/Types.hs
@@ -23,55 +23,10 @@ module HSChain.P2P.Types (
   ) where
 
 import Control.Exception      (throwIO,Exception)
-import Control.Monad.Catch    (MonadMask, MonadThrow)
 import Control.Monad.IO.Class (MonadIO(..))
-import qualified Data.ByteString.Lazy as LBS
 import qualified Network.Socket       as Net
 
 import HSChain.Network.Types
-
-
-----------------------------------------------------------------
---
-----------------------------------------------------------------
-
-sockAddrToNetAddr :: Net.SockAddr -> NetAddr
-sockAddrToNetAddr sa = case sa of
-  Net.SockAddrInet  port ha     -> NetAddrV4 ha $ fromIntegral port
-  Net.SockAddrInet6 port _ ha _ -> NetAddrV6 ha $ fromIntegral port
-  _                             -> error $ "unsupported socket address kind: "++show sa
-
-netAddrToSockAddr :: NetAddr -> Net.SockAddr
-netAddrToSockAddr (NetAddrV4 ha port) = Net.SockAddrInet  (fromIntegral port)  ha
-netAddrToSockAddr (NetAddrV6 ha port) = Net.SockAddrInet6 (fromIntegral port) 0 ha 0
-
--- | Network port
-type NetworkPort = Net.PortNumber
-
--- | Dictionary with API for network programming. We use it to be able
---   to provide two implementations of networking. One is real network
---   and another is mock in-process network for testing.
-data NetworkAPI = NetworkAPI
-  { listenOn :: !(forall m. (MonadIO m, MonadMask m)
-             => m (m (), m (P2PConnection, NetAddr)))
-    -- ^ Start listening on given port. Returns action to stop listener
-    --   and function for accepting new connections
-  , connect  :: !(forall m. (MonadIO m, MonadThrow m, MonadMask m)
-             => NetAddr -> m P2PConnection)
-    -- ^ Connect to remote address
-  , listenPort :: !NetworkPort
-    -- ^ Listen port.
-  }
-
-data P2PConnection = P2PConnection
-  { send          :: !(forall m. (MonadIO m) => LBS.ByteString -> m ())
-    -- ^ Send data
-  , recv          :: !(forall m. (MonadIO m) => m LBS.ByteString)
-    -- ^ Receive data. Will throw exception if connection is closed
-  , close         :: !(forall m. (MonadIO m) => m ())
-  }
-
-
 
 ----------------------------------------------------------------
 -- Hints
@@ -104,14 +59,3 @@ netAddrToAddrInfo addr = liftIO $ do
   return (ai,sockAddr,hostName)
   where
     sockAddr = netAddrToSockAddr addr
-
--- | Some network error.
-data NetworkError
-  = ConnectionTimedOut          -- ^ Connection timed out
-  | NoAddressAvailable          -- ^ 
-  | CantReverseLookipHostname   -- ^
-  | ConnectionClosed            -- ^
-  | ConnectionLoop
-  | SelfConnection
-  deriving stock    (Show)
-  deriving anyclass (Exception)
