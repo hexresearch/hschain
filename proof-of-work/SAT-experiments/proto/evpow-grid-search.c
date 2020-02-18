6a0f5b9f97ffdfd21854e001bc67fca6ffe6d3e7
Serguey Zefirov <sergueyz@gmail.com>
2020-02-18 16:26:40 +0300

    Search utility is under testing right now
    It looks like it is ready and works.

--- proof-of-work/SAT-experiments/proto/evpow-grid-search.c
+++ proof-of-work/SAT-experiments/proto/evpow-grid-search.c
@@ -17,9 +17,11 @@ typedef struct test_rec_s {
 
 void
 next_sample(test_rec_t* sample) {
-	int i;
-	for (i = 0; i < sizeof(sample->some_bytes); i ++) {
-		sample->some_bytes[i] += i * 2 + 1;
+	int i, carry;
+	for (i = 0, carry = 0; i < sizeof(sample->some_bytes); i ++) {
+		int r = sample->some_bytes[i] + i * 2 + 1 + carry;
+		carry = r >> 8;
+		sample->some_bytes[i] = r;
 	}
 } /* next_sample */
 
@@ -28,12 +30,13 @@ measure_average_solution_time
 	( int complexity_shift, uint16_t complexity_mantissa, test_rec_t* sample
 	, int64_t search_time_ms
 	, int64_t attempts_allowed
+	, int num_samples
 	) {
-	int found = 0;
-	int i, num_samples = 100;
+	int found;
+	int i;
 	clock_t start = clock(), end;
 	for (i = 0; i < num_samples; i ++) {
-		while(!found) {
+		do {	
 			next_sample(sample);
 			found = evpow_solve
 				( sample->some_bytes
@@ -49,18 +52,20 @@ measure_average_solution_time
         	                , 0
                 	        , NULL
 	                        );
-		}
-
+			printf("%c", found ? '+' : '-');
+		} while(!found);
 	}
 	end = clock();
-	return ((double)start - end)*1000.0/CLOCKS_PER_SEC/num_samples;
+	printf("\n");
+	return ((double)end - start)*1000.0/CLOCKS_PER_SEC/num_samples;
 } /* measure_average_solution_time */
 
 #define ARGS \
-	ARG(complexity_shift, int, 9) \
+	ARG(complexity_shift, int, 7) \
 	ARG(complexity_mantissa, uint16_t, 0xc000) \
 	ARG(search_time_ms, int64_t, 2000) \
 	ARG(attempts_allowed, uint64_t, 250000 * 2) \
+	ARG(num_samples, int, 100) \
 	ARG(nonce, uint64_t, 0xdeadbeef600df00dULL) \
 	ARG(break_power, double, 2.0) \
 	/* done with args */
@@ -107,10 +112,25 @@ parse_double(char*val) {
 		printf("not a double: '%s'\n", val);
 		exit(1);
 	}
+	return r;
 } /* parse_double */
 
+#define parse_int64_t parse_int
+#define parse_uint64_t parse_int
+#define parse_uint16_t parse_int
+int64_t
+parse_int(char*val) {
+	char* endptr = "<none>";
+	int64_t r = strtoll(val, &endptr, 0);
+	if (!*val || *endptr) {
+		printf("not an integer: %s/%s\n", val, endptr);
+		exit(1);
+	}
+	return r;
+} /* parse_int */
+
 void
-grid_search(int argc, char**argv) {
+parse_measure(int argc, char**argv) {
 	test_rec_t testrec;
 	int i;
 #define ARG(n, ty, val) ty n = val;
@@ -119,14 +139,6 @@ grid_search(int argc, char**argv) {
 	memset(&testrec, 0, sizeof(testrec));
 	testrec.some_bytes[0] = 123;
 	testrec.some_bytes[1] = 222; // for something fancier than just zeroes.
-	testrec.some_bytes[2] = nonce >>  0;
-	testrec.some_bytes[3] = nonce >>  8;
-	testrec.some_bytes[4] = nonce >> 16;
-	testrec.some_bytes[5] = nonce >> 24;
-	testrec.some_bytes[6] = nonce >> 32;
-	testrec.some_bytes[7] = nonce >> 40;
-	testrec.some_bytes[8] = nonce >> 48;
-	testrec.some_bytes[9] = nonce >> 56;
 	for (i = 1; i < argc; i++) {
 		char* a = argv[i];
 		if (a[0] != '-' || a[1] != '-') {
@@ -139,14 +151,29 @@ grid_search(int argc, char**argv) {
 			usage(argv[i]);
 		}
 	}
-} /* grid_search */
+	testrec.some_bytes[2] = nonce >>  0;
+	testrec.some_bytes[3] = nonce >>  8;
+	testrec.some_bytes[4] = nonce >> 16;
+	testrec.some_bytes[5] = nonce >> 24;
+	testrec.some_bytes[6] = nonce >> 32;
+	testrec.some_bytes[7] = nonce >> 40;
+	testrec.some_bytes[8] = nonce >> 48;
+	testrec.some_bytes[9] = nonce >> 56;
+	printf("%g\n", measure_average_solution_time
+			( complexity_shift, complexity_mantissa, &testrec
+			, search_time_ms
+			, attempts_allowed
+			, num_samples
+			)
+		);
+} /* parse_measure */
 
 
 int
 main(int argc, char**argv) {
         setvbuf(stdout, NULL, _IONBF, 0);
         setvbuf(stderr, NULL, _IONBF, 0);
-	grid_search(argc, argv);
+	parse_measure(argc, argv);
 	return 0;
 } /* main */
 
