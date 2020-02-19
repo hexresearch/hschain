fde20bf3bd4ec0b76c456ac90214d9b8eb7638cd
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-02-19 15:39:28 +0300

    Move tests for trees

--- hschain-types/test/Main.hs
+++ hschain-types/test/Main.hs
@@ -1,12 +1,8 @@
 import Test.Tasty
 
-import qualified TM.Merkle
-import qualified TM.MerkleBlock
 import qualified TM.Time
 
 main :: IO ()
 main = defaultMain $ testGroup "hschain-types"
-  [ TM.Merkle.tests
-  , TM.MerkleBlock.tests
-  , TM.Time.tests
+  [ TM.Time.tests
   ]
