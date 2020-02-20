56e7ffbe75f425a0ff2918b0ae18ac4028ae450c
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2020-02-20 11:18:38 +0300

    Move serialization tests for NetAddr to hschain-net (#561)
    * Move serialization tests to hshcain-net
    
    * Clean up cabal file

--- hschain-net/test/TM/Serialisation.hs
+++ hschain-net/test/TM/Serialisation.hs
@@ -15,17 +15,14 @@ module TM.Serialisation (tests) where
 
 import qualified Codec.Serialise as CBOR
 import qualified Data.Aeson      as JSON
-import Data.Coerce
 import Data.Typeable
 
 import Test.QuickCheck.Arbitrary
 import Test.Tasty
 import Test.Tasty.QuickCheck
 
-import HSChain.Types.Blockchain
-import HSChain.Mock.KeyVal    (BData(..))
+import HSChain.Network.Types  (NetAddr(..))
 
-import HSChain.Arbitrary.Instances ()
 
 ----------------------------------------------------------------
 -- Test tree
@@ -39,26 +36,7 @@ tests =
           run = testProperty (show (typeRep (Proxy @a)))
                                 (prop_CBOR_roundtrip @a)
       in testGroup "CBOR"
-         [ testGroup "roundtrip"
-           [ run @(Block  BData)
-           , run @(Header BData)
-           , run @(Commit BData)
-           , run @(ByzantineEvidence BData)
-           , run @(Proposal BData)
-           , run @(Vote 'PreVote   BData)
-           , run @(Vote 'PreCommit BData)
-           , run @Height
-           , run @Round
-           , run @Time
-           ]
-         , testGroup "Vote"
-           [ testProperty "serialize result for prevote and precommit differs"
-             prop_diff_serialize
-           , testProperty "cannot deserialise one vote type as other PV->PC"
-             (prop_diff_votes_dont_roundtrip @'PreVote)
-           , testProperty "cannot deserialise one vote type as other PC->PV"
-             (prop_diff_votes_dont_roundtrip @'PreCommit)
-           ]
+         [ run @NetAddr
          ]
     , let run :: forall a. ( Arbitrary a, Show a, Typeable a, Eq a
                            , JSON.FromJSON a, JSON.ToJSON a)
@@ -66,58 +44,20 @@ tests =
           run = testProperty (show (typeRep (Proxy @a)))
                                 (prop_JSON_roundtrip @a)
       in testGroup "JSON"
-         [ run @(Block  BData)
-         , run @(Header BData)
-         , run @(Commit BData)
-         , run @(ByzantineEvidence BData)
-         , run @(Proposal BData)
-         , run @(Vote 'PreVote   BData)
-         , run @(Vote 'PreCommit BData)
-         , run @Height
-         , run @Round
-         , run @Time
+         [ run @NetAddr
          ]
     , let run :: forall a. (Arbitrary a, Typeable a, Eq a, Show a, Read a)
               => TestTree
           run = testProperty (show (typeRep (Proxy @a)))
                                 (prop_ShowRead_roundtrip @a)
       in testGroup "Read/Show"
-         [ run @Height
-         , run @Round
-         , run @Time
+         [ run @NetAddr
+         , testProperty "NetAddr tcp://" $ \(a :: NetAddr) ->
+             a == (read . ("tcp://"++) . show) a
          ]
     ]
 
 ----------------------------------------------------------------
--- Tests
-----------------------------------------------------------------
-
-type TestVote ty   = Vote ty BData
-
-deriving newtype instance Arbitrary BData
-
-
--- Votes of one type can't be deserialized as other
-prop_diff_votes_dont_roundtrip
-  :: forall ty. ( CBOR.Serialise (TestVote ty)
-                , CBOR.Serialise (TestVote (OtherVote ty)))
-  => TestVote ty -> Bool
-prop_diff_votes_dont_roundtrip vote =
-  case CBOR.deserialiseOrFail (CBOR.serialise vote) of
-    Left   _                             -> True
-    Right (_ :: TestVote (OtherVote ty)) -> False
-
-type family OtherVote x where
-  OtherVote 'PreVote   = 'PreCommit
-  OtherVote 'PreCommit = 'PreVote
-
-
--- Prevotes and precommits are serialized differently
-prop_diff_serialize :: TestVote 'PreVote -> Bool
-prop_diff_serialize prevote
-  = CBOR.serialise prevote /= CBOR.serialise precommit
-  where
-    precommit = coerce prevote :: TestVote 'PreCommit
 
 -- CBOR.deserialise . CBOR.serialise
 prop_CBOR_roundtrip :: (Eq a, CBOR.Serialise a) => a -> Bool
@@ -130,3 +70,10 @@ prop_JSON_roundtrip a = Just a == (JSON.decode . JSON.encode) a
 -- Read . show = id
 prop_ShowRead_roundtrip :: (Eq a, Show a, Read a) => a -> Bool
 prop_ShowRead_roundtrip a = a == (read . show) a
+
+
+instance Arbitrary NetAddr where
+  arbitrary = oneof
+    [ NetAddrV4   <$> arbitrary <*> arbitrary
+    , NetAddrV6   <$> arbitrary <*> arbitrary
+    ]
