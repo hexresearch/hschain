1b8466be6d2d029dcf22d7d08ea9d0ddadc01c81
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-05-06 19:25:47 +0300

    Send announce to client once it connects
    This will allow it to sync immediately if needed

--- hschain-PoW/HSChain/PoW/P2P/Handler/Peer.hs
+++ hschain-PoW/HSChain/PoW/P2P/Handler/Peer.hs
@@ -60,6 +60,9 @@ runPeer conn chans@PeerChans{..} = logOnException $ do
                     peersBestHead   <- newTVarIO Nothing
                     inCatchup       <- newTVarIO False
                     return PeerState{..}
+  -- Send announce with current state at start
+  do s <- atomicallyIO peerConsensuSt
+     sinkIO sinkGossip $ GossipAnn $ AnnBestHead $ s ^. bestHead . _1 . to asHeader
   runConcurrently
     [ peerSend    conn (srcGossip <> fmap GossipAnn peerBCastAnn)
     , peerRecv    conn     st chans sinkGossip
