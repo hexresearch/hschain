f75fcc2b0a8f9d2d31dcf00beabbd1d4ff428841
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-06-03 16:23:18 +0300

    Test deriving

--- hschain-examples/exe/coin-node.hs
+++ hschain-examples/exe/coin-node.hs
@@ -2,9 +2,11 @@
 {-# LANGUAGE BangPatterns               #-}
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE DerivingVia                #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE RecordWildCards            #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
@@ -13,6 +15,7 @@
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE TypeOperators              #-}
 import Control.Concurrent
+import Control.Lens (Lens',lens,(%~))
 import Control.Monad
 import Control.Monad.Catch
 import Control.Monad.IO.Class
@@ -24,6 +27,7 @@ import Data.Word
 import Data.Yaml.Config       (loadYamlSettings, requireEnv)
 import Options.Applicative
 
+import Katip (Namespace,LogEnv,logF,Katip(..))
 import Network.Wai.Middleware.Prometheus
 import Prometheus   (register)
 import Prometheus.Metric.GHC
@@ -52,20 +56,69 @@ import HSChain.Network.Types  (NetAddr)
 --
 ----------------------------------------------------------------
 
-newtype MonitorT m a = MonitorT (ReaderT PrometheusGauges m a)
-  deriving ( Functor,Applicative,Monad
-           , MonadIO,MonadMask,MonadThrow,MonadCatch
-           , MonadLogger,MonadFork )
+data AppDict = AppDict
+  { dictGauges    :: !PrometheusGauges
+  , dictNamespace :: !Namespace
+  , dictLogEnv    :: !LogEnv
+  , dictConn      :: !(Connection 'RW BData)
+  }
 
-instance MonadIO m =>  MonadTMMonitoring (MonitorT m) where
-  usingCounter getter n = MonitorT $ flip addCounterNow n =<< asks getter
-  usingGauge   getter x = MonitorT $ flip setTGaugeNow x =<< asks getter
-  usingVector  getter l = MonitorT $ do
-    g <- asks getter
-    incTCVectorNow g l
+ldictNamespace :: Lens' AppDict Namespace
+ldictNamespace = lens dictNamespace (\d n -> d { dictNamespace = n })
 
-runMonitorT :: PrometheusGauges -> MonitorT m a -> m a
-runMonitorT g (MonitorT m) = runReaderT m g
+ldictLogEnv :: Lens' AppDict LogEnv
+ldictLogEnv = lens dictLogEnv (\d n -> d { dictLogEnv = n })
+
+
+newtype AppT m a = AppT { unAppT :: ReaderT AppDict m a }
+  deriving newtype (Functor,Applicative,Monad,MonadIO)
+  deriving newtype (MonadMask,MonadThrow,MonadCatch,MonadFork)
+
+runAppT :: LogEnv -> PrometheusGauges -> Connection 'RW BData -> AppT m a -> m a
+runAppT lenv g conn
+  = flip runReaderT AppDict { dictGauges    = g
+                            , dictNamespace = mempty
+                            , dictLogEnv    = lenv
+                            , dictConn      = conn
+                            }
+  . unAppT
+
+
+-- Tricky one.
+--  - Do we need Reader (Namespace,LogEnv)???
+--  - Do we need two readers???
+instance MonadIO m => Katip (AppT m) where
+  getLogEnv     = AppT $ asks dictLogEnv
+  localLogEnv f = AppT . local (ldictLogEnv %~ f) . unAppT
+
+instance MonadIO m => MonadLogger (AppT m) where
+  logger sev s a = do
+    nm <- AppT $ asks dictNamespace
+    logF a nm sev s
+  localNamespace f (AppT m) = AppT $ local (ldictNamespace %~ f) m
+
+-- Derived from single Reader
+instance MonadIO m => MonadTMMonitoring (AppT m) where
+  usingCounter getter n = AppT $ flip addCounterNow  n =<< asks (getter . dictGauges)
+  usingGauge   getter n = AppT $ flip setTGaugeNow   n =<< asks (getter . dictGauges)
+  usingVector  getter l = AppT $ flip incTCVectorNow l =<< asks (getter . dictGauges)
+
+
+-- Derived using Reader (Connection 'RW)
+instance Monad m => MonadReadDB (AppT m) BData where
+  askConnectionRO = AppT $ connectionRO <$> asks dictConn
+  
+instance Monad m => MonadDB (AppT m) BData where
+  askConnectionRW = AppT $ asks dictConn
+
+
+
+
+
+
+----------------------------------------------------------------
+--
+----------------------------------------------------------------
 
 data Opts = Opts
   { cmdConfigPath :: [FilePath]
@@ -109,7 +162,7 @@ main = do
     --- Allocate resources
     (conn, logenv) <- allocNode nodeSpec
     gauges         <- standardMonitoring
-    let run = runMonitorT gauges . runLoggerT logenv . runDBT conn
+    let run = runAppT logenv gauges conn
     -- Actually run node
     lift $ run $ do
       (RunningNode{..},acts) <- interpretSpec
