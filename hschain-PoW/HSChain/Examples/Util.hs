8df2028b8b72e6d199755844cf627bfc27a03893
Serguey Zefirov <sergueyz@gmail.com>
2020-06-08 01:11:33 +0300

    Remove redundant imports

--- hschain-PoW/HSChain/Examples/Util.hs
+++ hschain-PoW/HSChain/Examples/Util.hs
@@ -2,15 +2,11 @@
 -- |
 module HSChain.Examples.Util where
 
-import Control.Monad.IO.Class
-import Data.IORef
 import qualified Data.Map.Strict as Map
 
 import HSChain.PoW.Types
-import HSChain.PoW.Consensus
 import HSChain.Types.Merkle.Types
 import HSChain.Examples.Simple
-import HSChain.PoW.Node (inMemoryView, inMemoryDB)
 
 kvViewStep :: KVConfig cfg => Block (KV cfg) -> Map.Map Int String -> Maybe (Map.Map Int String)
 kvViewStep b m
