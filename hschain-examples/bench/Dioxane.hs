6135db571d93f1773679c2fd3a497ba5f5d146c9
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-07-03 15:50:19 +0300

    Fix benchamrks for dioxane

--- hschain-examples/bench/Dioxane.hs
+++ hschain-examples/bench/Dioxane.hs
@@ -6,10 +6,12 @@ module Dioxane (benchmarks) where
 import Codec.Serialise
 import Control.Arrow ((&&&))
 import Criterion.Main
+import Data.Functor.Identity
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Vector          as V
 
 import HSChain.Crypto
+import HSChain.Internal.Types.Consensus
 import HSChain.Mock.Dioxane
 import HSChain.Mock.KeyList
 import HSChain.Types
@@ -20,7 +22,7 @@ benchmarks :: Benchmark
 benchmarks
   = bgroup "Dioxane"
   [ bench "generate"    $ nf dioGenerate newBlock
-  , bench "process"     $ nf dioProcess (block1 <$ genesis)
+  , bench "process"     $ whnf dioProcess block1
   , bench "serialize"   $ nf serialise block1
   , bench "deserialize" $ nf (deserialise :: BL.ByteString -> Block (BData Tag)) (serialise block1)
   ]
@@ -54,25 +56,36 @@ newBlock = NewBlock
   , newBlockLastBID  = BlockID (Hashed (Hash ""))
   , newBlockCommit   = Nothing
   , newBlockEvidence = []
-  , newBlockState    = blockchainState genesis
-  , newBlockValSet   = merkleValue $ validatorSet genesis
+  , newBlockValSet   = genesisValSet genesis
   }
 
 block1 :: Block (BData Tag)
 block1 = Block
   { blockHeight        = Height 1
   , blockPrevBlockID   = Nothing
-  , blockValidators    = merkleHashed $ validatorSet genesis
-  , blockNewValidators = merkleHashed $ validatorSet genesis
+  , blockValidators    = valHash
+  , blockNewValidators = valHash
   , blockPrevCommit    = Nothing
   , blockEvidence      = merkled []
-  , blockData          = merkled $ bchValue dat
-  , blockStateHash     = merkleHashed $ blockchainState genesis
+  , blockData          = merkled dat
   }
-  where Just dat = dioGenerate newBlock
+  where
+    dat     = dioGenerate newBlock
+    valHash = hashed $ genesisValSet genesis
 
-dioGenerate :: NewBlock (BData Tag) -> Maybe (ProposedBlock (BData Tag))
-dioGenerate nb = generateBlock bchLogic nb []
+viewH0 :: StateView Identity (BData Tag)
+viewH0 = st1
+  where
+    Right st1 = runIdentity
+              $ validatePropBlock st0 (genesisBlock genesis) (genesisValSet genesis)
+    st0 = inMemoryStateView $ genesisValSet genesis
 
-dioProcess :: BlockValidation (BData Tag) -> Maybe (EvaluationResult (BData Tag))
-dioProcess = processBlock bchLogic
+dioGenerate :: NewBlock (BData Tag) -> BData Tag
+dioGenerate nb
+  = fst $ runIdentity
+  $ generateCandidate viewH0 nb
+
+dioProcess :: Block (BData Tag) -> Either (BChError (BData Tag)) (StateView Identity (BData Tag))
+dioProcess b
+  = runIdentity
+  $ validatePropBlock viewH0 b (genesisValSet genesis)
