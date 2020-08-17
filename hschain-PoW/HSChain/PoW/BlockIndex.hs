67ea4279564a2af605342ef6ba537681da8a93fe
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-08-17 13:33:52 +0300

    Add materialized path in block index

--- hschain-PoW/HSChain/PoW/BlockIndex.hs
+++ hschain-PoW/HSChain/PoW/BlockIndex.hs
@@ -2,8 +2,11 @@
 -- Block index is data structure which holds headers for all blocks in
 -- blockchain in memory. This module provides data structure and set
 -- of common operations for working with it.
-{-# LANGUAGE FlexibleContexts #-}
-module HSChain.PoW.BlockIndex  
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleContexts  #-}
+module HSChain.PoW.BlockIndex
   ( -- * Block index
     BlockIndex(..)
   , lookupIdx
@@ -12,6 +15,10 @@ module HSChain.PoW.BlockIndex
   , traverseBlockIndex
   , traverseBlockIndexM
   , traverseBlockIndexM_
+  -- ** Path in index
+  , BlockIndexPath(..)
+  , makeBlockIndexPath
+  , makeBlockIndexPathM
   -- * Construction of index
   , blockIndexHeads
   , blockIndexFromGenesis
@@ -125,3 +132,32 @@ blockIndexFromGenesis genesis
              , bhPrevious = Nothing
              , bhData     = merkleMap (const Proxy) $ blockData genesis
              }
+
+
+----------------------------------------------------------------
+-- Materialized path
+----------------------------------------------------------------
+
+-- | Path between blocks in block index
+data BlockIndexPath a
+  = RevertBlock !a (BlockIndexPath a)
+  | ApplyBlock  !a (BlockIndexPath a)
+  | NoChange
+  deriving (Show, Eq, Functor, Foldable, Traversable)
+
+makeBlockIndexPath
+  :: BlockData b => (BH b -> a) -> BH b -> BH b -> BlockIndexPath a
+makeBlockIndexPath f from to = traverseBlockIndex
+  (\bh -> RevertBlock (f bh))
+  (\bh -> ApplyBlock  (f bh))
+  from to
+  NoChange
+
+makeBlockIndexPathM
+  :: (Monad m, BlockData b)
+  => (BH b -> m a) -> BH b -> BH b -> m (BlockIndexPath a)
+makeBlockIndexPathM f from to = traverseBlockIndexM
+  (\bh p -> do { a <- f bh; return $ RevertBlock a p })
+  (\bh p -> do { a <- f bh; return $ ApplyBlock  a p })
+  from to
+  NoChange
