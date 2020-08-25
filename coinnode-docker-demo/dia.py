a9421a4d63cdfb8de2cf9d5d919724c2f7499242
romcheck <romcheck@gmail.com>
2020-08-25 10:14:20 +0200

    update docker registry image, add cluster scheme image

--- coinnode-docker-demo/dia.py
+++ coinnode-docker-demo/dia.py
@@ -3,13 +3,10 @@ from diagrams.aws.compute import EC2,ECS
 from diagrams.aws.general import User
 
 with Diagram("Simple Hschain cluster"):
-
-    with Cluster("blockchain network"):
-        with Cluster("validator nodes"):
-            validators = [EC2("validator 1"),
-                          EC2("validator 2"),
-                          EC2("validator 3"),
-                          EC2("validator 4")]
+    with Cluster("blockchain nodes"):
         explorer = ECS("block explorer")
+        with Cluster("validator nodes"):
+            EC2("validator 1") - EC2("validator 2") - EC2("validator 3") - EC2("validator 4")
 
-    User("") - explorer - validators
+
+    User("") >> explorer
