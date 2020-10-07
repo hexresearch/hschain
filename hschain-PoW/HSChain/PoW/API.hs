b87ea1575e9abeac1befe0499f42d48570f9fe59
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-10-07 12:47:16 +0300

    Add API endpoint which allows to check whether TX is accepted

--- hschain-PoW/HSChain/PoW/API.hs
+++ hschain-PoW/HSChain/PoW/API.hs
@@ -26,17 +26,21 @@ import HSChain.PoW.Types
 
 -- | REST API for interacting with mempool.
 data MempoolRestAPI b route = MempoolRestAPI
-  { mempoolPostTx :: route :- "post_tx"
-                           :> ReqBody '[JSON] (Tx b)
-                           :> Post '[PlainText] String
-
+  { mempoolPostTxAsync :: route :- "post_tx_async"
+                                :> ReqBody '[JSON] (Tx b)
+                                :> Post '[PlainText] String
+    -- | Post TX to mempool without checking whether it's accepted or not.
+  , mempoolPostTxSync  :: route :- "post_tx"
+                                :> ReqBody '[JSON] (Tx b)
+                                :> Post '[JSON] Bool
   }
   deriving Generic
 
 -- | Server implementation
 mempoolApiServer :: (MonadIO m) => MempoolAPI m b -> MempoolRestAPI b (AsServerT m)
 mempoolApiServer mempool = MempoolRestAPI
-  { mempoolPostTx = postTxEndpoint mempool
+  { mempoolPostTxAsync = postTxEndpoint     mempool
+  , mempoolPostTxSync  = postTxSyncEndpoint mempool
   }
 
 postTxEndpoint
@@ -45,6 +49,12 @@ postTxEndpoint
 postTxEndpoint MempoolAPI{..} tx = do
   sinkIO postTransaction tx
   return "TRIED"
+
+postTxSyncEndpoint
+  :: (MonadIO m)
+  => MempoolAPI m b -> Tx b -> m Bool
+postTxSyncEndpoint MempoolAPI{..} tx = do
+  blockingCall postTransactionSync tx
 
 
 ----------------------------------------------------------------
