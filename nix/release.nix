d6b75a25e5ccd34beddb34d8fe83ae500108169c
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-11-26 21:06:22 +0300

    Proof of work has been moved to hscahin-utxo

--- nix/release.nix
+++ nix/release.nix
@@ -48,7 +48,6 @@ let
     hschain-db             = callInternal hsPkgs "hschain" ../hschain-db             {} "";
     hschain-net            = callInternal hsPkgs "hschain" ../hschain-net            {} "";
     hschain-config         = callInternal hsPkgs "hschain" ../hschain-config         {} "";
-    hschain-pow-func       = callInternal hsPkgs "hschain" ../proof-of-work/full-mining {} "";
     hschain-pow-check      = callInternal hsPkgs "hschain" ../proof-of-work/lightweight-check {} "";
     hschain-PoW            = callInternal hsPkgs "hschain" ../hschain-PoW            {} "";
     hschain                = callInternal hsPkgs "hschain" ../hschain                {} "";
@@ -90,7 +89,6 @@ let
       hschain-control
       hschain-net
       hschain-db
-      hschain-pow-func
       hschain-pow-check
       hschain-PoW
       hschain
